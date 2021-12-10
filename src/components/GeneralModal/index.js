6453779d95bdd6f154d783f1c9e2f3b2000134fa
gerardo15 <gerardo_bastidas@hotmail.com>
2021-12-10 20:10:50 +0100

    scrollable fix

--- src/components/GeneralModal/index.js
+++ src/components/GeneralModal/index.js
@@ -4,49 +4,46 @@ import PropTypes from 'prop-types'
 import CloseIcon from '@material-ui/icons/Close'
 import { useStyles } from './styles'
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  bgcolor: 'background.paper',
-  border: 'none',
-  borderRadius: '10px',
-  boxShadow: 24,
-  pt: 2,
-  px: 4,
-  pb: 3,
-  "&:focus":{
-    outline: "none"
-  }
+function getModalStyle () {
+	const top = 50
+	const left = 50
+	return {
+		top: `${top}%`,
+		left: `${left}%`,
+		transform: `translate(-${top}%, -${left}%)`,
+		overflowY: 'scroll',
+		height: '70%',
+		width: '90%',
+		maxWidth: 1000,
+	}
 }
 
 const GeneralModal = ({ children, open, onClose, showClose }) => {
-  const classes = useStyles()
-  return (
-      <Modal
-        disableBackdropClick
-        open={open}
-        onClose={onClose}
-        aria-labelledby="parent-modal-title"
-        aria-describedby="parent-modal-description"
-      >
-        <Box sx={{ ...style, width: '90%', maxWidth: "648px" }}>
-          {showClose && (
-          <IconButton onClick={onClose} className={classes.closeButton}>
-            <CloseIcon />
-          </IconButton>
-          )}
-          {children}
-        </Box>
-      </Modal>
-  )
+	const classes = useStyles()
+	return (
+		<Modal
+			disableBackdropClick
+			open={open}
+			onClose={onClose}
+			aria-labelledby="parent-modal-title"
+			aria-describedby="parent-modal-description"
+		>
+			<div style={getModalStyle()} className={classes.paper}>
+				{showClose && (
+					<IconButton onClick={onClose} className={classes.closeButton}>
+						<CloseIcon />
+					</IconButton>
+				)}
+				{children}
+			</div>
+		</Modal>
+	)
 }
 
 GeneralModal.propTypes = {
-  children: PropTypes.element.isRequired,
-  open: PropTypes.bool.isRequired,
-  onClose: PropTypes.func.isRequired
+	children: PropTypes.element.isRequired,
+	open: PropTypes.bool.isRequired,
+	onClose: PropTypes.func.isRequired
 }
 
 export default GeneralModal
