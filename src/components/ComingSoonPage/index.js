cce54415f0b4bf8e56905584b5e45253808c17a3
mappedev <mappedev@gmail.com>
2022-01-31 17:17:11 -0400

    update: ComingSoon styles

--- src/components/ComingSoonPage/index.js
+++ src/components/ComingSoonPage/index.js
@@ -3,13 +3,13 @@ import React from 'react'
 import logoSrc from '../../../svg/logo.svg'
 
 export const ComingSoonPage = () => (
-  <div className="flex flex-col h-screen bg-center bg-cover bg-no-repeat bg-gray-100">
-		<div className="grid place-items-center w-4/5 mx-auto p-10 my-20 sm:my-auto bg-white-600 border-4 border-primary-500 bg-opacity-70 rounded-xl shadow-2xl space-y-5 text-center">
+  <div className="flex flex-col h-full bg-center bg-cover bg-no-repeat bg-gray-100">
+		<div className="grid place-items-center max-w-3xl w-full mx-auto p-10 my-20 sm:my-auto bg-white-600 border-4 border-primary-500 bg-opacity-70 rounded-xl shadow-2xl space-y-5 text-center">
       <img src={logoSrc} width={150} height={150} />
        
 			<h1 className="text-4xl font-bold uppercase text-primary-500 transition duration-500">Coming Soon</h1>
         <h2 className="text-xl text-gray-700 transition duration-500">We are almost there to introduce our new application to the world, in the meantime, you can follow us on social networks to get the latest updates.</h2>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        {/* <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
 
           <a 
            href="#" 
@@ -42,7 +42,7 @@ export const ComingSoonPage = () => (
             <span className="mx-auto">Reddit</span>
           </a>
 
-        </div>
+        </div> */}
 		</div>
 	</div>
 )
