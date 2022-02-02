28304d3c5242ae6f530eea6bcf2bb021e0cea0db
kevelscy <kevelscy@gmail.com>
2022-02-02 02:10:22 -0400

    add: border-radius in bar from loading page

--- components/layout/LoadingPage.tsx
+++ components/layout/LoadingPage.tsx
@@ -16,7 +16,7 @@ export const LoadingPage: FC<{ isRouteChanging: boolean }> = ({ isRouteChanging 
         }
 
         .bar {
-          background: #e73d23;
+          background: #ff2200;
           height: 8px;
           left: 0;
           margin-left: ${(-1 + progress) * 100}%;
@@ -25,7 +25,7 @@ export const LoadingPage: FC<{ isRouteChanging: boolean }> = ({ isRouteChanging 
           transition: margin-left ${animationDuration}ms linear;
           width: 100%;
           z-index: 1031;
-          border-radius: 0 1px 1px 0;
+          border-radius: 0 4px 4px 0;
         }
       `}</style>
       <div className='container'>
