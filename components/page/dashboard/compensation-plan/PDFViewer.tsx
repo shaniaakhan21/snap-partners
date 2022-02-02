456c36d450d173e548cc3940c13b193414fc375b
kevelscy <kevelscy@gmail.com>
2022-02-02 01:07:52 -0400

    update: rules eslint + validate windown in PDFViewer

--- components/page/dashboard/compensation-plan/PDFViewer.tsx
+++ components/page/dashboard/compensation-plan/PDFViewer.tsx
@@ -1,14 +1,22 @@
 import { Viewer, Worker, SpecialZoomLevel } from '@react-pdf-viewer/core'
 
 export const PDFViewer = () => {
+  if (window) {
+    return (
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.12.313/build/pdf.worker.js">
+        <div
+          className='mx-auto w-full'
+          style={{ height: '750px', maxWidth: 900 }}
+        >
+          <Viewer fileUrl="/static/plan.pdf" defaultScale={SpecialZoomLevel.PageFit} />
+        </div>
+      </Worker>
+    )
+  }
+
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.12.313/build/pdf.worker.js">
-      <div
-        className='mx-auto w-full'
-        style={{ height: '750px', maxWidth: 900 }}
-      >
-        <Viewer fileUrl="/static/plan.pdf" defaultScale={SpecialZoomLevel.PageFit} />
-      </div>
-    </Worker>
+    <div>
+      <span>Loading</span>
+    </div>
   )
 }
