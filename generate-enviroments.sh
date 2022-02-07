ccd936396410c6fad35037932b1320a53fa14409
gerardo15 <gerardo_bastidas@hotmail.com>
2022-02-07 21:25:20 +0100

    roll out enviroments

--- generate-enviroments.sh
+++ generate-enviroments.sh
@@ -45,7 +45,12 @@ server {
     }
     # Any route that doesn't have a file extension (e.g. /devices)
     location / {
-      try_files \$uri \$uri/ /index.html;
+      proxy_set_header Upgrade \$http_upgrade;
+      proxy_set_header Connection "upgrade";
+      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+      proxy_set_header Host \$host;
+      proxy_http_version 1.1;
+      proxy_pass http://localhost:4$PORT;
     }
 
     location ~ ^/api/(.+) {
