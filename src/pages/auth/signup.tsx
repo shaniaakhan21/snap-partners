c96a9d2dc954bf2576f98b971132ebbb0004ba06
kevelscy <kevelscy@gmail.com>
2022-02-13 02:06:48 -0800

    update: saving render conditon with dictionary roles

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -7,14 +7,15 @@ import { PAGE_INFO } from 'config/pageInfo'
 
 import { SignUpCustomerForm, SignUpDriverForm } from 'components/page/signup/SignUpForm'
 import { SelectRoleToSignUp } from 'components/page/signup/SelectRoleToSignUp'
+import { ROLES } from 'config/roles'
 
 const { SEO } = PAGE_INFO
 
 const SignUpPage: Page = () => {
   const { referralCode: code, referralRole: role } = useHandlerReferralLink()
 
-  if (role === 'CUSTOMER') return <SignUpCustomerForm referralLink={{ code, role }} />
-  if (role === 'DRIVER') return <SignUpDriverForm referralLink={{ code, role }} />
+  if (role === ROLES.CUSTOMER) return <SignUpCustomerForm referralLink={{ code, role }} />
+  if (role === ROLES.DRIVER) return <SignUpDriverForm referralLink={{ code, role }} />
 
   return <SelectRoleToSignUp />
 }
