5e6d58d71c074a2142d940695a39d4382e0b1714
mappedev <mappedev@gmail.com>
2022-02-16 17:16:27 -0400

    update: account settings

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,12 +1,23 @@
 import Head from 'next/head'
 import DashboardLayout from 'layouts/private/Dashboard'
-import type { Page as PageNext, ReactNode } from 'lib/types'
-import { PAGE_INFO } from 'config/pageInfo'
 import { AccountNoImage } from 'components/common/AccountNoImage'
+import { TrendingUpIcon } from 'components/common/icons'
+import { PAGE_INFO } from 'config/pageInfo'
+import type { Page as PageNext, ReactNode } from 'lib/types'
+import { useAuthStore } from 'lib/stores'
 
 const { SEO } = PAGE_INFO
 
 const AccountPage: PageNext = () => {
+  const { auth } = useAuthStore()
+
+  const handleClickUpgradeToManager = () => {
+    window.open(
+      `https://store.snapdelivered.com/product/manager-upgrade?userId=${auth.id}`,
+      'noopener'
+    )
+  }
+
   return (
     <>
       <Head>
@@ -15,6 +26,27 @@ const AccountPage: PageNext = () => {
 
       <div className='flex justify-center items-center'>
         <AccountNoImage />
+      </div>
+
+      <div className='flex items-center justify-center mt-11 h-20'>
+        <div className='w-1/2 h-full flex items-center bg-white rounded px-4 mr-4'>
+          <div className='bg-warning-300 w-12 h-12 flex items-center justify-center mr-4'>
+            <TrendingUpIcon />
+          </div>
+
+          <div>
+            <span className='block text-gray-400 text-sm'>Rank</span>
+            <span className='text-lg font-semibold'>Director</span>
+          </div>
+        </div>
+
+        <button
+          onClick={handleClickUpgradeToManager}
+          className='w-1/2 h-full bg-textAcent-500 text-white rounded shadow-md flex flex-col justify-center px-4'
+        >
+          <span className='text-sm'>Best Plan</span>
+          <h6 className='text-lg font-semibold'>Upgrade to manager</h6>
+        </button>
       </div>
     </>
   )
