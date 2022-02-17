06ebbcdcf7af6fe1d8aab62422b07eee2b4b8850
Javid <cavid.hacizade.99@gmail.com>
2022-02-17 22:51:41 +0400

    reverted test changes

--- src/components/page/signup/SignUpForm/FormByRole/utils/Steps/SuccessCode.tsx
+++ src/components/page/signup/SignUpForm/FormByRole/utils/Steps/SuccessCode.tsx
@@ -12,7 +12,7 @@ export const SuccessCode = ({ userTrack, handleStep }: { userTrack: IUserTrack, 
       <span className='text-4xl font-bold text-primary-500'>Register Done!</span>
       <CheckSuccess classes='my-10' />
       <span className='text-xl text-primary-500'>Welcome!</span>
-      {/* <span className='text-4xl text-primary-500 font-bold'>{userTrack.userInfo.name}</span> */}
+      <span className='text-4xl text-primary-500 font-bold'>{userTrack.userInfo.name}</span>
 
       <Button onClick={() => handleStep(STEPS.UPGRADE_TO_MANAGER)} classes='w-full mt-10'>CONTINUE</Button>
       <br/>
