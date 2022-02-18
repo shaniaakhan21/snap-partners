8b7059b778e5beaa1d7a704a7a3bc4eae18e2df5
kevelscy <kevelscy@gmail.com>
2022-02-18 15:47:09 -0400

    update: new fields to Account Page

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import DashboardLayout from 'layouts/private/Dashboard'
-import { EditIcon, GenealogyIcon, TrendingUpIcon } from 'components/common/icons'
+import { EditIcon, GenealogyIcon } from 'components/common/icons'
 import { useAuthStore } from 'lib/stores'
 import { PAGE_INFO } from 'config/pageInfo'
 import type { Page as PageNext, ReactNode } from 'lib/types'
@@ -40,29 +40,96 @@ const AccountPage: PageNext = () => {
       </div>
 
       <div className='mt-11'>
-        <ul className='bg-white w-full h-full rounded-lg'>
-          <li className='px-4 py-3 border-y-2 border-y-gray-200 flex justify-between items-center'>
-            <div>
-              <label htmlFor='email' className='text-sm'>Email</label>
-              <br />
-              <input
-                id='email'
-                name='email'
-                type='email'
-                value={auth.email}
-                disabled={true}
-                className='w-full bg-transparent text-lg truncate'
-              />
-            </div>
+        <ul className='w-full h-full rounded-lg'>
+          <div className='flex flex-col sm:flex-row justify-start items-start gap-y-2 gap-x-2'>
+            <li className='rounded-xl bg-white w-full sm:w-1/2 px-4 py-3 border-y-2 border-y-gray-200 flex justify-between items-center'>
+              <div>
+                <label htmlFor='name' className='text-sm'>Name</label>
+                <br />
+                <input
+                  id='name'
+                  name='name'
+                  type='text'
+                  value={auth.name}
+                  disabled={true}
+                  className='w-full bg-transparent text-lg truncate'
+                />
+              </div>
 
-            <div>
-              <button className='bg-gray-300 rounded-full px-4 py-1 text-white font-bold uppercase'>
+              <div>
+                <button className='bg-gray-300 rounded-full px-4 py-1 text-white font-bold uppercase'>
+                  Edit
+                </button>
+              </div>
+            </li>
+
+            <li className='rounded-xl bg-white w-full sm:w-1/2 px-4 py-3 border-y-2 border-y-gray-200 flex justify-between items-center'>
+              <div>
+                <label htmlFor='lastname' className='text-sm'>Last Name</label>
+                <br />
+                <input
+                  id='lastname'
+                  name='lastname'
+                  type='text'
+                  value={auth.lastname}
+                  disabled={true}
+                  className='w-full bg-transparent text-lg truncate'
+                />
+              </div>
+
+              <div>
+                <button className='bg-gray-300 rounded-full px-4 py-1 text-white font-bold uppercase'>
+                  Edit
+                </button>
+              </div>
+            </li>
+          </div>
+
+          <div className='flex flex-col sm:flex-row justify-start items-start mt-2 gap-y-2 gap-x-2'>
+            <li className='rounded-xl bg-white w-full sm:w-1/2 px-4 py-3 border-y-2 border-y-gray-200 flex justify-between items-center'>
+              <div>
+                <label htmlFor='name' className='text-sm'>Phone</label>
+                <br />
+                <input
+                  id='phone'
+                  name='phone'
+                  type='tel'
+                  value={auth.phone}
+                  disabled={true}
+                  className='w-full bg-transparent text-lg truncate'
+                />
+              </div>
+
+              <div>
+                <button className='bg-gray-300 rounded-full px-4 py-1 text-white font-bold uppercase'>
+                  Edit
+                </button>
+              </div>
+            </li>
+
+            <li className='w-full sm:w-1/2 rounded-xl bg-white px-4 py-3 border-y-2 border-y-gray-200 flex justify-between items-center'>
+              <div>
+                <label htmlFor='email' className='text-sm'>Email</label>
+                <br />
+                <input
+                  id='email'
+                  name='email'
+                  type='email'
+                  value={auth.email}
+                  disabled={true}
+                  className='w-full bg-transparent text-lg truncate'
+                />
+              </div>
+
+              <div>
+                <button className='bg-gray-300 rounded-full px-4 py-1 text-white font-bold uppercase'>
                 Edit
-              </button>
-            </div>
-          </li>
+                </button>
+              </div>
+            </li>
+          </div>
 
-          <li className='px-4 py-3 border-y-2 border-y-gray-200 flex justify-between items-center'>
+          <li className='rounded-xl bg-white px-4 py-3 mt-2 border-y-2 border-y-gray-200 flex justify-between items-center'>
             <div>
               <label htmlFor='email' className='text-sm'>Password</label>
               <br />
@@ -83,7 +150,7 @@ const AccountPage: PageNext = () => {
             </div>
           </li>
 
-          <li className='px-4 py-3 border-y-2 border-y-gray-200 flex justify-between items-center'>
+          <li className='rounded-xl bg-white px-4 py-3 mt-2 border-y-2 border-y-gray-200 flex justify-between items-center'>
             <div>
               <label htmlFor='bankAccount' className='text-sm'>Bank Account</label>
               <br />
@@ -107,7 +174,7 @@ const AccountPage: PageNext = () => {
       </div>
 
       <div className='flex flex-col md:flex-row items-center justify-center mt-11'>
-        <div className='w-full md:w-1/2 h-20 flex items-center bg-white rounded-lg px-4 mr-0 md:mr-4 mb-4 md:mb-0'>
+        {/* <div className='w-full md:w-1/2 h-20 flex items-center bg-white rounded-lg px-4 mr-0 md:mr-4 mb-4 md:mb-0'>
           <div className='bg-warning-300 rounded-lg w-12 h-12 flex items-center justify-center mr-4'>
             <TrendingUpIcon />
           </div>
@@ -116,7 +183,7 @@ const AccountPage: PageNext = () => {
             <span className='block text-gray-400 text-sm'>Rank</span>
             <span className='text-lg font-semibold'>Director</span>
           </div>
-        </div>
+        </div> */}
 
         <button
           onClick={handleClickUpgradeToManager}
