3e0f623e44d20df9afa62c5b06390b544d6d6857
kevelscy <kevelscy@gmail.com>
2022-02-23 10:11:32 -0400

    update: Footer links & background

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,8 +8,8 @@ export const Footer = () => {
     { to: '#', label: 'Merch Store' },
     { to: '#', label: 'Opportunities with Snap Delivered' },
     { to: '#', label: 'Support' },
-    { to: '#', label: 'Privacy policy' },
-    { to: '#', label: 'Terms of Use' }
+    { to: '/legal/privacy-policy', label: 'Privacy policy' },
+    { to: '/legal/terms-of-use', label: 'Terms of Use' }
   ])
 
   const { current: socialLinks } = useRef([
@@ -19,14 +19,14 @@ export const Footer = () => {
   ])
 
   return (
-    <footer className='w-full bg-gray-300 p-4'>
-      <div className='max-w-7xl mx-auto 2xl:flex 2xl:justify-start 2xl:items-center 2xl:gap-x-3'>
+    <footer className='w-full bg-transparent p-4 text-sm'>
+      <div className='max-w-7xl mx-auto 2xl:flex 2xl:justify-center 2xl:items-center 2xl:gap-x-3'>
         <ul className='grid place-content-center grid-flow-row md:grid-rows-2 grid-cols-1 md:grid-cols-3 2xl:flex 2xl:justify-start 2xl:items-start 2xl:gap-x-4 text-gray-800 gap-y-2 2xl:mr-3'>
           {
             footerLinks.map(footerLink => (
               <li key={footerLink.label}>
                 <Link href={footerLink.to}>
-                  <a>{footerLink.label}</a>
+                  <a className='hover:text-primary-500'>{footerLink.label}</a>
                 </Link>
               </li>
             ))
@@ -45,7 +45,7 @@ export const Footer = () => {
           </ul>
 
           <div>
-            <span className='text-gray-800 text-xs'>© 2022 Snap Delivered. All rights reserved.</span>
+            <span className='text-gray-800'>© 2022 Snap Delivered. All rights reserved.</span>
           </div>
         </section>
       </div>
