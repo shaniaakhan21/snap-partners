cbfdd557a8c34c10042eb61be4054534f08d1b02
mappedev <mappedev@gmail.com>
2022-03-08 11:25:42 -0400

    update: reset password page, SSR token

--- src/pages/auth/reset-password.tsx
+++ src/pages/auth/reset-password.tsx
@@ -1,15 +1,26 @@
-import { useRouter } from 'next/router'
+import { GetServerSideProps, GetServerSidePropsContext } from 'next'
 
-const ResetPasswordPage = () => {
-  const router = useRouter()
-
-  console.log(router.query)
-
+const ResetPasswordPage = ({ token }) => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <h1 className="text-3xl text-center">RESET PASSWORD PAGE</h1>
-    </div>
+    <section className="w-screen h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl text-center font-bold">RESET PASSWORD PAGE</h1>
+      <p className='text-xl mt-8'>Token: {token}</p>
+    </section>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }: GetServerSidePropsContext) => {
+  const token = query?.token
+
+  if (!token) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: { token }
+  }
 }
 
 export default ResetPasswordPage
