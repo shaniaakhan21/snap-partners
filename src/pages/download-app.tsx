05f67f5c39b3fe0d38101ce090ac47136112a141
kevelscy <kevelscy@gmail.com>
2022-03-09 01:20:33 -0400

    update: link apps to download

--- src/pages/download-app.tsx
+++ src/pages/download-app.tsx
@@ -22,19 +22,19 @@ const DownloadAppPage: PageNext = () => {
       title: 'Driver app',
       subtitle: 'Snap Operations Specialist app',
       description: 'Fullfil and Manage orders and',
-      link: '#'
+      link: 'https://play.google.com/store/apps/details?id=com.snapdelivered.driverapp'
     },
     {
       title: 'Costumer app',
       subtitle: 'User app',
       description: 'Order food and get it SNAP',
-      link: '#'
+      link: 'https://play.google.com/store/apps/details?id=com.snapdelivered.userapp'
     },
     {
       title: 'Restaurant app',
       subtitle: 'Snap Merchant Control app',
       description: 'Manager orders and payments',
-      link: '#'
+      link: 'https://play.google.com/store/apps/details?id=com.snapdelivered.restaurantapp'
     }
   ])
 
@@ -43,19 +43,19 @@ const DownloadAppPage: PageNext = () => {
       title: 'Driver app',
       subtitle: 'Snap Operations Specialist app',
       description: 'Fullfil and Manage orders and',
-      link: '#'
+      link: 'https://apps.apple.com/us/app/snap-operations-specialist/id1478862279'
     },
     {
       title: 'Costumer app',
       subtitle: 'User app',
       description: 'Order food and get it SNAP',
-      link: '#'
+      link: 'https://apps.apple.com/us/app/snap-user/id1478863057'
     },
     {
       title: 'Restaurant app',
       subtitle: 'Snap Merchant Control app',
       description: 'Manager orders and payments',
-      link: '#'
+      link: 'https://apps.apple.com/ve/app/snap-merchant-control/id1478737288'
     }
   ])
 
@@ -90,13 +90,13 @@ const DownloadAppPage: PageNext = () => {
           <div className='mt-5 flex justify-center items-center'>
             <Link href='/download-app?device=ANDROID'>
               <a className='block w-36 mr-8 px-4 py-2 text-lg font-bold disabled:opacity-50 disabled:cursor-not-allowed bg-black-primary text-white bg-primary-500 rounded-full focus:outline-none focus:ring focus:ring-primary-300 focus:opacity-90 hover:opacity-90'>
-              Android
+                Android
               </a>
             </Link>
 
             <Link href='/download-app?device=APPLE'>
               <a className='block w-36 px-4 py-2 text-lg font-bold disabled:opacity-50 disabled:cursor-not-allowed bg-black-primary text-white bg-primary-500 rounded-full focus:outline-none focus:ring focus:ring-primary-300 focus:opacity-90 hover:opacity-90'>
-              iOs
+                iOs
               </a>
             </Link>
           </div>
@@ -153,7 +153,7 @@ const DownloadAppPage: PageNext = () => {
                       <p className='text-gray-700'>{androidApp.description}</p>
 
                       <a
-                        href='#'
+                        href={androidApp.link}
                         target='_blank'
                         rel='noopener noreferrer'
                         className='block w-fit mt-4'
@@ -171,7 +171,7 @@ const DownloadAppPage: PageNext = () => {
                       <p className='text-gray-700'>{appleApp.description}</p>
 
                       <a
-                        href='#'
+                        href={appleApp.link}
                         target='_blank'
                         rel='noopener noreferrer'
                         className='block w-fit mt-4'
