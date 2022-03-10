805f554f043d86607a7faf1452daef8e3454e3ef
kevelscy <kevelscy@gmail.com>
2022-03-10 15:32:45 -0400

    update: api destinaton config to dev

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ module.exports = {
         {
           source: '/api/:path*',
           // destination: `http://localhost:${API.BACKEND_PORT}/:path*`,
-          destination: 'https://snapdeliveredteam.com/api/:path*'
-          // destination: 'http://dev.snap.devopsteam.info/api/:path*'
+          // destination: 'https://snapdeliveredteam.com/api/:path*'
+          destination: 'http://dev.snap.devopsteam.info/api/:path*'
         }
       ]
       : []
