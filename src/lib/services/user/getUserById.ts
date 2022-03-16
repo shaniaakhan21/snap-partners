2d7753245738194e79f275bf53c2c87baa2da5dd
mappedev <mappedev@gmail.com>
2022-03-16 17:45:41 -0400

    update: getUserById to User folder

---
+++ src/lib/services/user/getUserById.ts
@@ -0,0 +1,38 @@
+import { IQueryErrorReturn } from 'lib/types/http/query'
+
+interface IQueryReturn extends IQueryErrorReturn {
+  data: {
+    id: 1,
+    email: string,
+    phoneNumber: string,
+    sponsorId: number | null,
+    'sponsor.id': null,
+    'sponsor.name': null
+  } | null
+}
+// ? This interface will be change.
+
+export const getUserById = async (id: number | string, token: string): Promise<IQueryReturn> => {
+  const res = await fetch(`/api/user/getUserById?id=${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
+  const data = await res.json()
+
+  if (!res.ok) {
+    return {
+      data: null,
+      error: {
+        info: data.error,
+        status: res.status
+      }
+    }
+  }
+
+  return {
+    data: data.data,
+    error: null
+  }
+}
