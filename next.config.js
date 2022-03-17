ee211d348a3f607541e8a88963436f5a9f11a62f
kevelscy <kevelscy@gmail.com>
2022-03-16 20:37:37 -0400

    update: https in the api url next.config

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = {
           source: '/api/:path*',
           // destination: `http://localhost:${API.BACKEND_PORT}/:path*`,
           // destination: 'https://snapdeliveredteam.com/api/:path*'
-          destination: 'http://dev.snap.devopsteam.info/api/:path*'
+          destination: 'https://dev.snap.devopsteam.info/api/:path*'
         }
       ]
       : []
