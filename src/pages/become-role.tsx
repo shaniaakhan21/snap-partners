e56d0a3505f27ee5cb8eb6272402450cbdbce320
mappedev <mappedev@gmail.com>
2022-03-23 12:53:41 -0400

    add: input file to become drive

--- src/pages/become-role.tsx
+++ src/pages/become-role.tsx
@@ -14,8 +14,12 @@ import { useAuthStore } from 'lib/stores'
 import { handleFetchError } from 'lib/utils/handleFetchError'
 import { updateUserRole } from 'lib/services/user/updateUserRole'
 import { Button } from 'components/common/Button'
+import { InputFile } from 'components/page/signup/SignUpForm/FormByRole/utils/InputFile'
+import { DownloadIcon } from 'components/common/icons'
 
 const { SEO } = APP_INFO
+
+const maxFileSizeInMb = 5
 
 const BecomeRolePage = ({ role }: { role: 'CUSTOMER' | 'DRIVER' | 'RESTAURANT' }) => { // check the types
   const { handleSubmit, reset, register } = useForm()
@@ -29,6 +33,24 @@ const BecomeRolePage = ({ role }: { role: 'CUSTOMER' | 'DRIVER' | 'RESTAURANT' }
   // }, [])
 
   const onSubmit = async (dataForm) => {
+    // if (dataForm.idImage && dataForm.idImage[0].size > (maxFileSizeInMb * 1000000)) {
+    //   setError('idImage', { message: `The maximum file size in ID Image is ${maxFileSizeInMb}mb, please upload a file with a maximum file size of ${maxFileSizeInMb}mb` })
+    //   setLoading(false)
+    //   return
+    // }
+
+    // if (dataForm.driverLicense && dataForm.driverLicense[0].size > (maxFileSizeInMb * 1000000)) {
+    //   setError('insuranceImage', { message: `The maximum file size in Driver License is ${maxFileSizeInMb}mb, please upload a file with a maximum file size of ${maxFileSizeInMb}mb` })
+    //   setLoading(false)
+    //   return
+    // }
+
+    // if (dataForm.carInsurance && dataForm.carInsurance[0].size > (maxFileSizeInMb * 1000000)) {
+    //   setError('carInsurance', { message: `The maximum file size in Car Insurance is ${maxFileSizeInMb}mb, please upload a file with a maximum file size of ${maxFileSizeInMb}mb` })
+    //   setLoading(false)
+    //   return
+    // }
+
     const { error } = await updateUserRole(dataForm, auth.accessToken)
 
     if (error) {
@@ -48,7 +70,7 @@ const BecomeRolePage = ({ role }: { role: 'CUSTOMER' | 'DRIVER' | 'RESTAURANT' }
     router.push('/overview')
   }
 
-  if (role === 'CUSTOMER' || role === 'DRIVER') {
+  if (role.toLocaleUpperCase() === 'CUSTOMER' || role.toLocaleUpperCase() === 'DRIVER') {
     return (
       <form onSubmit={handleSubmit(onSubmit)} className='max-w-xl mx-auto'>
         <h5 className='font-bold text-2xl text-primary-500'>
@@ -109,6 +131,54 @@ const BecomeRolePage = ({ role }: { role: 'CUSTOMER' | 'DRIVER' | 'RESTAURANT' }
           {...register('phoneNumber')}
           disabled
           className='select-none w-full px-3 py-1 my-2 text-base text-gray-700 bg-gray-100 border border-gray-300 rounded outline-none appearance-none bg-opacity-50 focus:border-brown-primary-500 focus:bg-white focus:ring-2 focus:ring-brown-primary-300 leading-8 transition-colors duration-200 ease-in-out'
+        />
+
+        <label htmlFor='idImage' className='font-bold text-gray-700 uppercase text-sm'>ID</label>
+        <span className='block text-gray-800 text-sm'>Format JPG, JPEG, PNG, WEBP and PDF Max 5mb</span>
+        <input
+          id='idImage'
+          name='idImage'
+          type="file"
+          accept=".jpg, .jpeg, .png, .webp, .pdf"
+          // {...register(registerId, rulesForm)}
+          className="block w-1/2 my-2 text-sm text-slate-500 cursor-pointer
+          file:mr-4 file:py-2 file:px-2
+          file:rounded-full file:border-0
+          file:text-xs file:font-bold file:uppercase
+          file:bg-gray-500 file:text-white
+          file:w-1/2 hover:file:opacity-90"
+        />
+
+        <label htmlFor='driverLicense' className='font-bold text-gray-700 uppercase text-sm'>Driver License</label>
+        <span className='block text-gray-800 text-sm'>Format JPG, JPEG, PNG, WEBP and PDF Max 5mb</span>
+        <input
+          id='driverLicense'
+          name='driverLicense'
+          type="file"
+          accept=".jpg, .jpeg, .png, .webp, .pdf"
+          // {...register(registerId, rulesForm)}
+          className="block w-1/2 my-2 text-sm text-slate-500 cursor-pointer
+          file:mr-4 file:py-2 file:px-2
+          file:rounded-full file:border-0
+          file:text-xs file:font-bold file:uppercase
+          file:bg-gray-500 file:text-white
+          file:w-1/2 hover:file:opacity-90"
+        />
+
+        <label htmlFor='carInsurance' className='font-bold text-gray-700 uppercase text-sm'>Insurance Image</label>
+        <span className='block text-gray-800 text-sm'>Format JPG, JPEG, PNG, WEBP and PDF Max 5mb</span>
+        <input
+          id='carInsurance'
+          name='carInsurance'
+          type="file"
+          accept=".jpg, .jpeg, .png, .webp, .pdf"
+          // {...register(registerId, rulesForm)}
+          className="block w-1/2 my-2 text-sm text-slate-500 cursor-pointer
+          file:mr-4 file:py-2 file:px-2
+          file:rounded-full file:border-0
+          file:text-xs file:font-bold file:uppercase
+          file:bg-gray-500 file:text-white
+          file:w-1/2 hover:file:opacity-90"
         />
 
         <Button classes='w-full mt-4'>
