2f1092cd792c209e55fd2e29aa4ca0ee29b1488d
kevelscy <kevelscy@gmail.com>
2022-03-29 16:21:03 -0400

    update: modal container scroll

--- src/components/common/ModalContainer.tsx
+++ src/components/common/ModalContainer.tsx
@@ -7,7 +7,7 @@ interface IProps {
 export const ModalContainer = ({ children }: IProps) => {
   return (
     <div
-      className='w-[calc(100vw-20px)] max-w-[600px] max-h-[calc(85vh-60px)] rounded-sm shadow-xl bg-white p-4 overflow-auto'
+      className='w-[calc(100vw-20px)] max-w-[600px] max-h-[calc(85vh-60px)] rounded-sm shadow-xl bg-white p-4 overflow-auto scroll-primary'
     >
       {children}
     </div>
--- src/components/page/referrals/UserDetailModal.tsx
+++ src/components/page/referrals/UserDetailModal.tsx
@@ -38,74 +38,76 @@ export const ReferralsUserDetailModal = ({ id, name, email, phone, sponsor, rank
 
   return (
     <div className='p-1 text-xs sm:text-sm lg:text-base'>
-      <div className='mb-4 flex items-center justify-between'>
-        <div>
-          {auth.roles.admin && (
-            <UpdateUserRank
-              id={id}
-              rank={rank}
-              authToken={auth.accessToken}
-            />
-          )}
+      <div>
+        <div className='mb-4 flex items-center justify-between'>
+          <div>
+            {auth.roles.admin && (
+              <UpdateUserRank
+                id={id}
+                rank={rank}
+                authToken={auth.accessToken}
+              />
+            )}
+          </div>
+
+          <div className={`flex mt-0 sm:mt-8 ${auth.roles.admin ? 'flex-col items-end sm:flex-row sm:items-center justify-end' : 'items-center justify-end'}`}>
+            <span className='text-primary-500 font-bold'>USER ID</span>
+
+            <button
+              onClick={() => copy(id.toString(), 'ID')}
+              className={`ml-3 text-blue-600 flex items-end transition-colors hover:text-blue-600 lg:text-textHint ${auth.roles.admin ? 'flex-col sm:flex-row sm:items-center' : 'items-center'}`}
+            >
+              <span className={`${auth.roles.admin ? 'sm:mr-3' : 'mr-3'}`}>{id}</span>
+              <CopyIcon classes="w-4 h-4 sm:w-5 sm:h-5" />
+            </button>
+          </div>
         </div>
 
-        <div className={`flex mt-0 sm:mt-8 ${auth.roles.admin ? 'flex-col items-end sm:flex-row sm:items-center justify-end' : 'items-center justify-end'}`}>
-          <span className='text-primary-500 font-bold'>USER ID</span>
+        <hr className='my-4 bordet-t border-gray-300' />
 
-          <button
-            onClick={() => copy(id.toString(), 'ID')}
-            className={`ml-3 text-blue-600 flex items-end transition-colors hover:text-blue-600 lg:text-textHint ${auth.roles.admin ? 'flex-col sm:flex-row sm:items-center' : 'items-center'}`}
-          >
-            <span className={`${auth.roles.admin ? 'sm:mr-3' : 'mr-3'}`}>{id}</span>
-            <CopyIcon classes="w-4 h-4 sm:w-5 sm:h-5" />
-          </button>
-        </div>
-      </div>
-
-      <hr className='my-4 bordet-t border-gray-300' />
-
-      <div className='w-full'>
-        <div className='flex justify-between items-start'>
-          <p className='mb-4 block text-primary-500 font-bold'>{name.toUpperCase()}</p>
-          <ul className='flex justify-end gap-x-2'>
-            <UserBadges userRank={rank} userRoles={roles} />
-          </ul>
-        </div>
-      </div>
-
-      <div className='mb-8 flex flex-col sm:flex-row justify-center sm:items-center sm:justify-between'>
-        <div className='inline-flex items-center'>
-          <EmailIcon classes='w-5 h-5' />
-          <span className='ml-1 text-blue-600'>{email}</span>
+        <div className='w-full'>
+          <div className='flex justify-between items-start'>
+            <p className='mb-4 block text-primary-500 font-bold'>{name.toUpperCase()}</p>
+            <ul className='flex justify-end gap-x-2'>
+              <UserBadges userRank={rank} userRoles={roles} />
+            </ul>
+          </div>
         </div>
 
-        <div className='items-center hidden sm:inline-flex'>
-          <PhoneIcon classes='w-5 h-5' />
-          <span className='ml-1 text-blue-600'>{phone}</span>
+        <div className='mb-8 flex flex-col sm:flex-row justify-center sm:items-center sm:justify-between'>
+          <div className='inline-flex items-center'>
+            <EmailIcon classes='w-5 h-5' />
+            <span className='ml-1 text-blue-600'>{email}</span>
+          </div>
+
+          <div className='items-center hidden sm:inline-flex'>
+            <PhoneIcon classes='w-5 h-5' />
+            <span className='ml-1 text-blue-600'>{phone}</span>
+          </div>
         </div>
-      </div>
 
-      <div className='flex justify-between items-center'>
-        {
-          sponsor
-            ? (
-              <p className='inline-block'>
-              Sponsored by {' '}
-                <span className='inline-block text-primary-500'>
-                  {`${sponsor.name} ${sponsor.lastname ? sponsor.lastname : ''}`}
-                </span>
-              </p>
-            )
-            : (
-              <p className='inline-block'>No Sponsored</p>
-            )
-        }
+        <div className='flex justify-between items-center'>
+          {
+            sponsor
+              ? (
+                <p className='inline-block'>
+                Sponsored by {' '}
+                  <span className='inline-block text-primary-500'>
+                    {`${sponsor.name} ${sponsor.lastname ? sponsor.lastname : ''}`}
+                  </span>
+                </p>
+              )
+              : (
+                <p className='inline-block'>No Sponsored</p>
+              )
+          }
 
-        <Link href='/glosary'>
-          <a onClick={closeModalManually}>
-            <InfoIcon />
-          </a>
-        </Link>
+          <Link href='/glosary'>
+            <a onClick={closeModalManually}>
+              <InfoIcon />
+            </a>
+          </Link>
+        </div>
       </div>
 
       <hr className='my-4 bordet-t border-gray-300' />
