1b3d3e1ba92833b7f1b8be08046d5cea4048ce3c
gerardo15 <gerardo_bastidas@hotmail.com>
2022-04-07 22:22:27 +0200

    hide recognition

--- src/components/layout/private/Dashboard/Drawer/routes.tsx
+++ src/components/layout/private/Dashboard/Drawer/routes.tsx
@@ -41,11 +41,6 @@ export const drawerRoutes = [
     to: 'https://snapdelivered.ourproshop.com/',
     label: 'Tools'
   },
-  {/*
-    icon: <RelojTempIcon />,
-    to: '/comingsoon',
-    label: 'Recognition'
-  */},
   {
     icon: <GenealogyIcon />,
     to: '/genealogy',
