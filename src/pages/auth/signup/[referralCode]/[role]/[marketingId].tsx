a03926ced4fbc99bff2c8d8afd655d6c29d08441
gerardo15 <gerardo_bastidas@hotmail.com>
2022-04-11 22:15:06 +0200

    use and log params

--- src/pages/auth/signup/[referralCode]/[role]/[marketingId].tsx
+++ src/pages/auth/signup/[referralCode]/[role]/[marketingId].tsx
@@ -14,53 +14,55 @@ import { ROLES } from 'config/roles'
 const { SEO } = APP_INFO
 
 const SignUpPage = () => {
-    const { referralCode: code, role } = useHandlerReferralLink()
+  const { referralCode: code, role } = useHandlerReferralLink()
 
-    if (role === ROLES.CUSTOMER) return <SignUpCustomerForm referralLink={{ code, role }} />
-    if (role === ROLES.DRIVER) return <SignUpDriverForm referralLink={{ code, role }} />
-    if (role === ROLES.MERCHANT) return <SignUpMerchantForm referralLink={{ code, role }} />
+  if (role === ROLES.CUSTOMER) return <SignUpCustomerForm referralLink={{ code, role }} />
+  if (role === ROLES.DRIVER) return <SignUpDriverForm referralLink={{ code, role }} />
+  if (role === ROLES.MERCHANT) return <SignUpMerchantForm referralLink={{ code, role }} />
 
-    return <SelectRoleToSignUp />
+  return <SelectRoleToSignUp />
 }
 
 SignUpPage.getLayout = (page) => (
-    <AuthPagesLayout>
-        <Head>
-            <title>{SEO.TITLE_PAGE} - Sign Up</title>
-            {
-                page.props.rrssInfo && (
-                    <>
-                        <meta property='og:url' content={`${APP_INFO.SEO.URL_PAGE}/auth/signup`} />
-                        <meta property='og:image' content={page.props.rrssInfo.imageId} />
-                        <meta property='twitter:image' content={page.props.rrssInfo.imageId} />
-                    </>
-                )
-            }
-        </Head>
+  <AuthPagesLayout>
+    <Head>
+      <title>{SEO.TITLE_PAGE} - Sign Up</title>
+      {
+        page.props.rrssInfo && (
+          <>
+            <meta property='og:url' content={`${APP_INFO.SEO.URL_PAGE}/auth/signup`} />
+            <meta property='og:image' content={page.props.rrssInfo.imageId} />
+            <meta property='twitter:image' content={page.props.rrssInfo.imageId} />
+          </>
+        )
+      }
+    </Head>
 
-        {page}
-    </AuthPagesLayout>
+    {page}
+  </AuthPagesLayout>
 )
 
-export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-    const marketingId = query.marketingId
+export const getServerSideProps: GetServerSideProps = async ({ query, params }) => {
+  const marketingId = params.marketingId
 
-    if (marketingId) {
-        const res = await fetch(`${APP_INFO.SEO.URL_PAGE}/api/marketing/${marketingId}`)
-        const { data } = await res.json()
+  if (marketingId) {
+    const res = await fetch(`${APP_INFO.SEO.URL_PAGE}/api/marketing/${marketingId}`)
+    const { data } = await res.json()
 
-        return {
-            props: {
-                rrssInfo: data
-            }
-        }
-    } else {
-        return {
-            props: {
-                rrssInfo: null
-            }
-        }
+    return {
+      props: {
+        rrssInfo: data,
+        params
+      }
     }
+  } else {
+    return {
+      props: {
+        rrssInfo: null,
+        params
+      }
+    }
+  }
 }
 
 export default SignUpPage
