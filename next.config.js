464e3bf607c1e0271c70f8ffc503e6e582d9dc8b
mappedev <mappedev@gmail.com>
2022-05-26 09:50:59 -0400

    update: compensation plan

--- next.config.js
+++ next.config.js
@@ -3,16 +3,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async rewrites () {
+  async rewrites() {
     return process.env.NODE_ENV !== 'production'
       ? [
         {
           source: '/api/:path*',
           // destination: `http://localhost:${API.BACKEND_PORT}/:path*`
-          // destination: 'https://snapdeliveredteam.com/api/:path*'
+          destination: 'https://snapdeliveredteam.com/api/:path*'
           // destination: 'https://devstage.snap.devopsteam.info/api/:path*'
           // destination: 'https://dev.snap.devopsteam.info/api/:path*'
-          destination: 'https://snap421.snap.devopsteam.info/api/:path*'
+          // destination: 'https://snap421.snap.devopsteam.info/api/:path*'
         }
       ]
       : []
