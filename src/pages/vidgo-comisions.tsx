620b1d912cc6195a2bdd05e16572692541327d35
kevelscy <kevelscy@gmail.com>
2022-07-22 13:34:27 -0400

    lint

--- src/pages/vidgo-comisions.tsx
+++ src/pages/vidgo-comisions.tsx
@@ -1,42 +1,42 @@
-import Head from "next/head";
-import { useEffect, useState } from "react";
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
 
-import type { Page, ReactNode } from "lib/types";
-import { useAuthStore } from "lib/stores";
-import { APP_INFO } from "config/appInfo";
+import type { Page, ReactNode } from 'lib/types'
+import { useAuthStore } from 'lib/stores'
+import { APP_INFO } from 'config/appInfo'
 
-import DashboardLayout from "layouts/private/Dashboard";
-import { BonusBenmarks } from "components/page/vidgo-comisions/BonusBenmarksTable";
-import { RetentionMonthly } from "components/page/vidgo-comisions/RetentionMonthly";
-import { CustomerTable } from "components/page/vidgo-comisions/CustomerTable";
-import { TeamCustomerTable } from "components/page/vidgo-comisions/TeamCustomerTable";
-import { TableRetentionBonusTracker } from "components/page/vidgo-comisions/TableRetentionBonusTracker";
+import DashboardLayout from 'layouts/private/Dashboard'
+import { BonusBenmarks } from 'components/page/vidgo-comisions/BonusBenmarksTable'
+import { RetentionMonthly } from 'components/page/vidgo-comisions/RetentionMonthly'
+import { CustomerTable } from 'components/page/vidgo-comisions/CustomerTable'
+import { TeamCustomerTable } from 'components/page/vidgo-comisions/TeamCustomerTable'
+import { TableRetentionBonusTracker } from 'components/page/vidgo-comisions/TableRetentionBonusTracker'
 
-import { commissions } from "lib/services/vidgo/commissions";
-import { handleFetchError } from "lib/utils/handleFetchError";
-import { DataUsage } from "@material-ui/icons";
+import { commissions } from 'lib/services/vidgo/commissions'
+import { handleFetchError } from 'lib/utils/handleFetchError'
+import { DataUsage } from '@material-ui/icons'
 
-const { SEO } = APP_INFO;
+const { SEO } = APP_INFO
 
 const dataReport = {
   teamCustomers: [
     {
-      label: "Level 1",
+      label: 'Level 1',
       count: 10,
-      comission: 13,
-    },
+      comission: 13
+    }
   ],
 
   myCustomers: [
     {
       level: 1,
-      customerName: "customer1",
-      customerEmail: "customer@gmail.com",
-      package: "Package 1",
-      joinDate: "01/01/2020",
-      lastPayment: "01/01/2020",
-      monthlyEarning: 4,
-    },
+      customerName: 'customer1',
+      customerEmail: 'customer@gmail.com',
+      package: 'Package 1',
+      joinDate: '01/01/2020',
+      lastPayment: '01/01/2020',
+      monthlyEarning: 4
+    }
   ],
 
   bonusTrackerMonthly: {
@@ -45,7 +45,7 @@ const dataReport = {
       month2: 0,
       month3: 0,
       month6: 0,
-      month12: 0,
+      month12: 0
     },
 
     eligibleBenchmark: {
@@ -53,7 +53,7 @@ const dataReport = {
       month2: 0,
       month3: 0,
       month6: 0,
-      month12: 0,
+      month12: 0
     },
 
     payOfTotal: {
@@ -61,7 +61,7 @@ const dataReport = {
       month2: 0,
       month3: 0,
       month6: 0,
-      month12: 0,
+      month12: 0
     },
 
     commission: {
@@ -69,140 +69,140 @@ const dataReport = {
       month2: 0,
       month3: 0,
       month6: 0,
-      month12: 0,
-    },
+      month12: 0
+    }
   },
 
   activePayments: [
     {
-      customerName: "customer1",
+      customerName: 'customer1',
       month1: 0,
       month2: 0,
       month3: 0,
       month6: 0,
-      month12: 0,
-    },
-  ],
-};
+      month12: 0
+    }
+  ]
+}
 
-var monthNames = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
 
 const VidgoComisionsPage: Page = () => {
-  const [vidgoReport, setVidgoReport] = useState(null);
-  const [dateSelected, setDateSelected] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const { auth } = useAuthStore();
-  const [customers, setCustomers] = useState([]);
-  const [teamCustomers, setTeamCustomers] = useState([]);
-  const [monthlyReport, setMonthlyReport] = useState([]);
-  const [dates, setDates] = useState([]);
+  const [vidgoReport, setVidgoReport] = useState(null)
+  const [dateSelected, setDateSelected] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const { auth } = useAuthStore()
+  const [customers, setCustomers] = useState([])
+  const [teamCustomers, setTeamCustomers] = useState([])
+  const [monthlyReport, setMonthlyReport] = useState([])
+  const [dates, setDates] = useState([])
 
   useEffect(() => {
     (async () => {
-      setLoading(true);
-      const { data, error } = await commissions(auth.accessToken);
+      setLoading(true)
+      const { data, error } = await commissions(auth.accessToken)
 
       if (error) {
-        handleFetchError(error.status, error.info);
-        setLoading(false);
-        return;
+        handleFetchError(error.status, error.info)
+        setLoading(false)
+        return
       }
 
-      setVidgoReport(data);
-      setLoading(false);
-    })();
-  }, []);
+      setVidgoReport(data)
+      setLoading(false)
+    })()
+  }, [])
 
   const handleChange = (event) => {
-    setDateSelected(event.target.value);
-  };
+    setDateSelected(event.target.value)
+  }
 
   const getMonthName = (date, fullName) => {
-    var monthName = monthNames[date];
-    return fullName ? monthName : monthName.substring(0, 3);
-  };
+    const monthName = monthNames[date]
+    return fullName ? monthName : monthName.substring(0, 3)
+  }
 
   useEffect(() => {
     if (vidgoReport) {
-      let customs = [];
-      let teams = [];
-      let months = [];
+      const customs = []
+      const teams = []
+      const months = []
       vidgoReport.forEach((element) => {
         customs.push({
-          customerName: element["name"],
-          customerEmail: element["email"],
-          package: element["package"],
-          joinDate: element["joinDate"],
-          lastPayment: element["paymentDate"],
-          monthlyEarning: element["total"],
-        });
+          customerName: element.name,
+          customerEmail: element.email,
+          package: element.package,
+          joinDate: element.joinDate,
+          lastPayment: element.paymentDate,
+          monthlyEarning: element.total
+        })
         teams.push({
-          name: element["name"],
-          level: "3",
-          customerCount: "3",
-          comission: "2444",
-        });
-        let rowDate = new Date(element["joinDate"]);
-        let name =
+          name: element.name,
+          level: '3',
+          customerCount: '3',
+          comission: '2444'
+        })
+        const rowDate = new Date(element.joinDate)
+        const name =
           getMonthName(rowDate.getMonth() + 1, monthNames) +
-          " " +
-          rowDate.getFullYear();
-        let value = new Date(
+          ' ' +
+          rowDate.getFullYear()
+        const value = new Date(
           rowDate.getFullYear(),
           rowDate.getMonth() + 1,
           1
-        ).toDateString();
-        let monthObj = {
+        ).toDateString()
+        const monthObj = {
           name: name,
-          value: value,
-        };
-        if (!months.some((e) => e.value === value)) {
-          months.push(monthObj);
+          value: value
         }
-      });
-      setDates(months);
-      setCustomers(customs);
-      setTeamCustomers(teams);
+        if (!months.some((e) => e.value === value)) {
+          months.push(monthObj)
+        }
+      })
+      setDates(months)
+      setCustomers(customs)
+      setTeamCustomers(teams)
     }
-  }, [vidgoReport]);
+  }, [vidgoReport])
 
   useEffect(() => {
     if (vidgoReport) {
-      let nextMonth = new Date(
+      const nextMonth = new Date(
         new Date(dateSelected).getFullYear(),
         new Date(dateSelected).getMonth() + 1,
         1
-      );
-      let report = vidgoReport.filter((item: any) => {
+      )
+      const report = vidgoReport.filter((item: any) => {
         return (
           new Date(item.paymentDate).getTime() <
             new Date(dateSelected).getTime() ||
           new Date(item.paymentDate).getTime() > nextMonth.getTime()
-        );
-      });
-      setMonthlyReport(report);
+        )
+      })
+      setMonthlyReport(report)
     }
-  }, [dateSelected]);
+  }, [dateSelected])
 
   if (!auth.roles.merchant) {
     return (
       <div className="h-screen-80 w-full flex justify-center items-center">
         <span className="text-4xl font-black">Should be a IBO</span>
       </div>
-    );
+    )
   }
 
   return (
@@ -272,8 +272,8 @@ const VidgoComisionsPage: Page = () => {
         </section>
       </div>
     )
-  );
-};
+  )
+}
 
 VidgoComisionsPage.getLayout = (page: ReactNode) => (
   <DashboardLayout>
@@ -282,6 +282,6 @@ VidgoComisionsPage.getLayout = (page: ReactNode) => (
     </Head>
     {page}
   </DashboardLayout>
-);
+)
 
-export default VidgoComisionsPage;
+export default VidgoComisionsPage
