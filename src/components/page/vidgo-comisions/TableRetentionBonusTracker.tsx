25796178b9ac8f428ef6d8252177ddf421f8424f
hernansamper95 <hernansamper95@gmail.com>
2022-07-22 03:19:54 -0500

    WIP fetch / parse sheet into table data

--- src/components/page/vidgo-comisions/TableRetentionBonusTracker.tsx
+++ src/components/page/vidgo-comisions/TableRetentionBonusTracker.tsx
@@ -1,93 +1,139 @@
-export const TableRetentionBonusTracker = () => {
+export const TableRetentionBonusTracker = ({ report }: { report: any[] }) => {
   return (
     <>
-      <div className='overflow-x-scroll scroll-x-primary w-full h-full'>
-        <table className='table-auto w-full bg-white mt-4'>
+      <div className="overflow-x-scroll scroll-x-primary w-full h-full">
+        <table className="table-auto w-full bg-white mt-4">
           <thead>
             <tr>
-              <th className='w-52'>&nbsp;</th>
-              <th className='px-2'>Month 1</th>
-              <th className='px-2'>Month 2</th>
-              <th className='px-2'>Month 3</th>
-              <th className='px-2'>Month 6</th>
-              <th className='px-2'>Month 12</th>
+              <th className="w-52">&nbsp;</th>
+              <th className="px-2">Month 1</th>
+              <th className="px-2">Month 2</th>
+              <th className="px-2">Month 3</th>
+              <th className="px-2">Month 6</th>
+              <th className="px-2">Month 12</th>
             </tr>
           </thead>
           <tbody>
-            <tr className='border-t border-zinc-300'>
-              <td className='px-4 py-2 border-r border-zinc-300'>Total Active Count</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2'>0</td>
+            <tr className="border-t border-zinc-300">
+              <td className="px-4 py-2 border-r border-zinc-300">
+                Total Active Count
+              </td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2">0</td>
             </tr>
 
-            <tr className='border-t border-zinc-300'>
-              <td className='px-4 py-2 border-r border-zinc-300'>Eligible Benchmark</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2'>0</td>
+            <tr className="border-t border-zinc-300">
+              <td className="px-4 py-2 border-r border-zinc-300">
+                Eligible Benchmark
+              </td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2">0</td>
             </tr>
 
-            <tr className='border-t border-zinc-300'>
-              <td className='px-4 py-2 border-r border-zinc-300'>% Pay of Total</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2'>0</td>
+            <tr className="border-t border-zinc-300">
+              <td className="px-4 py-2 border-r border-zinc-300">
+                % Pay of Total
+              </td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2">0</td>
             </tr>
 
-            <tr className='border-t border-zinc-300'>
-              <td className='px-4 py-2 border-r border-zinc-300'>Commission</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-              <td className='text-center px-4 py-2'>0</td>
+            <tr className="border-t border-zinc-300">
+              <td className="px-4 py-2 border-r border-zinc-300">Commission</td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2 border-r border-zinc-300">
+                0
+              </td>
+              <td className="text-center px-4 py-2">0</td>
             </tr>
           </tbody>
         </table>
       </div>
 
-      <section className='text-center mt-4'>
-        <h5 className='text-lg font-bold'>Active Payments</h5>
+      <section className="text-center mt-4">
+        <h5 className="text-lg font-bold">Active Payments</h5>
       </section>
 
-      <div className='overflow-x-scroll scroll-x-primary w-full h-full'>
-        <table className='table-auto w-full bg-white mt-2 overflow-y-scroll'>
+      <div className="overflow-x-scroll scroll-x-primary w-full h-full">
+        <table className="table-auto w-full bg-white mt-2 overflow-y-scroll">
           <thead>
             <tr>
-              <th className='w-52'>&nbsp;</th>
-              <th className='px-4'>Month 1</th>
-              <th className='px-4'>Month 2</th>
-              <th className='px-4'>Month 3</th>
-              <th className='px-4'>Month 6</th>
-              <th className='px-4'>Month 12</th>
+              <th className="w-52">&nbsp;</th>
+              <th className="px-4">Month 1</th>
+              <th className="px-4">Month 2</th>
+              <th className="px-4">Month 3</th>
+              <th className="px-4">Month 6</th>
+              <th className="px-4">Month 12</th>
             </tr>
           </thead>
 
           <tbody>
-            {
-              [1, 2, 3, 4, 5, 6, 7, 8, 9].map((mount) => {
-                return (
-                  <tr className='border-t border-zinc-300' key={mount}>
-                    <td className='px-4 py-2 border-r border-zinc-300'>Customer</td>
-                    <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-                    <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-                    <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-                    <td className='text-center px-4 py-2 border-r border-zinc-300'>0</td>
-                    <td className='text-center px-4 py-2'>0</td>
-                  </tr>
-                )
-              })
-            }
+            {report.map((customer) => {
+              return (
+                <tr className="border-t border-zinc-300" key={customer.id}>
+                  <td className="px-4 py-2 border-r border-zinc-300">
+                    {customer.name}
+                  </td>
+                  <td className="text-center px-4 py-2 border-r border-zinc-300">
+                    0
+                  </td>
+                  <td className="text-center px-4 py-2 border-r border-zinc-300">
+                    0
+                  </td>
+                  <td className="text-center px-4 py-2 border-r border-zinc-300">
+                    0
+                  </td>
+                  <td className="text-center px-4 py-2 border-r border-zinc-300">
+                    0
+                  </td>
+                  <td className="text-center px-4 py-2">0</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
     </>
-  )
-}
+  );
+};
--- src/pages/vidgo-comisions.tsx
+++ src/pages/vidgo-comisions.tsx
@@ -1,194 +1,283 @@
-import Head from 'next/head'
-// import { useState } from 'react'
+import Head from "next/head";
+import { useEffect, useState } from "react";
 
-import type { Page, ReactNode } from 'lib/types'
-// import { useAuthStore } from 'lib/stores'
-import { APP_INFO } from 'config/appInfo'
+import type { Page, ReactNode } from "lib/types";
+import { useAuthStore } from "lib/stores";
+import { APP_INFO } from "config/appInfo";
 
-import DashboardLayout from 'layouts/private/Dashboard'
-// import { BonusBenmarks } from 'components/page/vidgo-comisions/BonusBenmarksTable'
-// import { RetentionMonthly } from 'components/page/vidgo-comisions/RetentionMonthly'
-// import { CustomerTable } from 'components/page/vidgo-comisions/CustomerTable'
-// import { TeamCustomerTable } from 'components/page/vidgo-comisions/TeamCustomerTable'
-// import { TableRetentionBonusTracker } from 'components/page/vidgo-comisions/TableRetentionBonusTracker'
-import ComingSoon from './comingsoon'
+import DashboardLayout from "layouts/private/Dashboard";
+import { BonusBenmarks } from "components/page/vidgo-comisions/BonusBenmarksTable";
+import { RetentionMonthly } from "components/page/vidgo-comisions/RetentionMonthly";
+import { CustomerTable } from "components/page/vidgo-comisions/CustomerTable";
+import { TeamCustomerTable } from "components/page/vidgo-comisions/TeamCustomerTable";
+import { TableRetentionBonusTracker } from "components/page/vidgo-comisions/TableRetentionBonusTracker";
 
-const { SEO } = APP_INFO
+import { commissions } from "lib/services/vidgo/commissions";
+import { handleFetchError } from "lib/utils/handleFetchError";
+import { DataUsage } from "@material-ui/icons";
 
-// const dataReport = {
-//   teamCustomers: [
-//     {
-//       label: 'Level 1',
-//       count: 10,
-//       comission: 13
-//     }
-//   ],
+const { SEO } = APP_INFO;
 
-//   myCustomers: [
-//     {
-//       level: 1,
-//       customerName: 'customer1',
-//       customerEmail: 'customer@gmail.com',
-//       package: 'Package 1',
-//       joinDate: '01/01/2020',
-//       lastPayment: '01/01/2020',
-//       monthlyEarning: 4
-//     }
-//   ],
+const dataReport = {
+  teamCustomers: [
+    {
+      label: "Level 1",
+      count: 10,
+      comission: 13,
+    },
+  ],
 
-//   bonusTrackerMonthly: {
-//     totalActiveCount: {
-//       month1: 0,
-//       month2: 0,
-//       month3: 0,
-//       month6: 0,
-//       month12: 0
-//     },
+  myCustomers: [
+    {
+      level: 1,
+      customerName: "customer1",
+      customerEmail: "customer@gmail.com",
+      package: "Package 1",
+      joinDate: "01/01/2020",
+      lastPayment: "01/01/2020",
+      monthlyEarning: 4,
+    },
+  ],
 
-//     eligibleBenchmark: {
-//       month1: 0,
-//       month2: 0,
-//       month3: 0,
-//       month6: 0,
-//       month12: 0
-//     },
+  bonusTrackerMonthly: {
+    totalActiveCount: {
+      month1: 0,
+      month2: 0,
+      month3: 0,
+      month6: 0,
+      month12: 0,
+    },
 
-//     payOfTotal: {
-//       month1: 0,
-//       month2: 0,
-//       month3: 0,
-//       month6: 0,
-//       month12: 0
-//     },
+    eligibleBenchmark: {
+      month1: 0,
+      month2: 0,
+      month3: 0,
+      month6: 0,
+      month12: 0,
+    },
 
-//     commission: {
-//       month1: 0,
-//       month2: 0,
-//       month3: 0,
-//       month6: 0,
-//       month12: 0
-//     }
-//   },
+    payOfTotal: {
+      month1: 0,
+      month2: 0,
+      month3: 0,
+      month6: 0,
+      month12: 0,
+    },
 
-//   activePayments: [
-//     {
-//       customerName: 'customer1',
-//       month1: 0,
-//       month2: 0,
-//       month3: 0,
-//       month6: 0,
-//       month12: 0
-//     },
-//     {
-//       customerName: 'customer3',
-//       month1: 0,
-//       month2: 0,
-//       month3: 0,
-//       month6: 0,
-//       month12: 0
-//     },
-//     {
-//       customerName: 'customer2',
-//       month1: 0,
-//       month2: 0,
-//       month3: 0,
-//       month6: 0,
-//       month12: 0
-//     }
-//   ]
-// }
+    commission: {
+      month1: 0,
+      month2: 0,
+      month3: 0,
+      month6: 0,
+      month12: 0,
+    },
+  },
+
+  activePayments: [
+    {
+      customerName: "customer1",
+      month1: 0,
+      month2: 0,
+      month3: 0,
+      month6: 0,
+      month12: 0,
+    },
+  ],
+};
+
+var monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 const VidgoComisionsPage: Page = () => {
-  // const [vidgoReport] = useState(dataReport)
-  // const [dateSelected, setDateSelected] = useState('')
-  // const { auth } = useAuthStore()
+  const [vidgoReport, setVidgoReport] = useState(null);
+  const [dateSelected, setDateSelected] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const { auth } = useAuthStore();
+  const [customers, setCustomers] = useState([]);
+  const [teamCustomers, setTeamCustomers] = useState([]);
+  const [monthlyReport, setMonthlyReport] = useState([]);
+  const [dates, setDates] = useState([]);
 
-  // const handleChange = (event) => {
-  //   setDateSelected(event.target.value)
-  // }
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const { data, error } = await commissions(auth.accessToken);
+
+      if (error) {
+        handleFetchError(error.status, error.info);
+        setLoading(false);
+        return;
+      }
+
+      setVidgoReport(data);
+
+      let customs = [];
+      let teams = [];
+      let months = [];
+      data.forEach((element) => {
+        customs.push({
+          customerName: element["name"],
+          customerEmail: element["email"],
+          package: element["package"],
+          joinDate: element["joinDate"],
+          lastPayment: element["paymentDate"],
+          monthlyEarning: element["total"],
+        });
+        teams.push({
+          name: element["name"],
+          level: "3",
+          customerCount: "3",
+          comission: "2444",
+        });
+        let rowDate = new Date(element["joinDate"]);
+        let name =
+          getMonthName(rowDate.getMonth() + 1, monthNames) +
+          " " +
+          rowDate.getFullYear();
+        let value = new Date(
+          rowDate.getFullYear(),
+          rowDate.getMonth() + 1,
+          1
+        ).toDateString();
+        let monthObj = {
+          name: name,
+          value: value,
+        };
+        if (!months.some((e) => e.value === value)) {
+          months.push(monthObj);
+        }
+      });
+      setDates(months);
+      setCustomers(customs);
+      setTeamCustomers(teams);
+      setLoading(false);
+    })();
+  }, []);
+
+  const handleChange = (event) => {
+    setDateSelected(event.target.value);
+  };
+
+  const getMonthName = (date, fullName) => {
+    var monthName = monthNames[date];
+    return fullName ? monthName : monthName.substring(0, 3);
+  };
+
+  useEffect(() => {
+    if (vidgoReport) {
+      let nextMonth = new Date(
+        new Date(dateSelected).getFullYear(),
+        new Date(dateSelected).getMonth() + 1,
+        1
+      );
+      let report = vidgoReport.filter((item: any) => {
+        return (
+          new Date(item.paymentDate).getTime() <
+            new Date(dateSelected).getTime() ||
+          new Date(item.paymentDate).getTime() > nextMonth.getTime()
+        );
+      });
+      setMonthlyReport(report);
+    }
+  }, [dateSelected]);
+
+  if (!auth.roles.merchant) {
+    return (
+      <div className="h-screen-80 w-full flex justify-center items-center">
+        <span className="text-4xl font-black">Should be a IBO</span>
+      </div>
+    );
+  }
 
   return (
-    <ComingSoon />
-  )
+    vidgoReport && (
+      <div>
+        <section className="w-full h-full flex flex-col-reverse lg:flex-row justify-between items-center lg:items-start gap-x-8 gap-y-8">
+          <div className="overflow-x-scroll scroll-x-primary w-full lg:w-2/3 h-full">
+            <h5 className="text-lg font-semibold">Your Customers</h5>
 
-  // if (!auth.roles.merchant) {
-  //   return (
-  //     <div className='h-screen-80 w-full flex justify-center items-center'>
-  //       <span className='text-4xl font-black'>Should be a IBO</span>
-  //     </div>
-  //   )
-  // }
+            <CustomerTable myCustomers={customers} />
+          </div>
 
-  // return (
-  //   <div>
-  //     <section className='w-full h-full flex flex-col-reverse lg:flex-row justify-between items-center lg:items-start gap-x-8 gap-y-8'>
-  //       <div className='overflow-x-scroll scroll-x-primary w-full lg:w-2/3 h-full'>
-  //         <h5 className='text-lg font-semibold'>
-  //           Your Customers
-  //         </h5>
+          <div className="w-full lg:w-1/3">
+            <div className="flex justify-between items-start">
+              <h5 className="text-lg font-semibold">Team Customers</h5>
 
-  //         <CustomerTable myCustomers={vidgoReport.myCustomers} />
-  //       </div>
+              <div className="flex justify-end items-center gap-x-4 text-right">
+                <span className="text-xs">
+                  Total Downline <br /> Customers
+                </span>
 
-  //       <div className='w-full lg:w-1/3'>
-  //         <div className='flex justify-between items-start'>
-  //           <h5 className='text-lg font-semibold'>
-  //             Team Customers
-  //           </h5>
+                <div className="border border-zinc-400 rounded-sm px-4 py-1">
+                  <span className="text-xl font-semibold">1766</span>
+                </div>
+              </div>
+            </div>
 
-  //           <div className='flex justify-end items-center gap-x-4 text-right'>
-  //             <span className='text-xs'>Total Downline <br /> Customers</span>
+            <TeamCustomerTable teamCustomers={teamCustomers} />
+          </div>
+        </section>
 
-  //             <div className='border border-zinc-400 rounded-sm px-4 py-1'>
-  //               <span className='text-xl font-semibold'>
-  //                 1766
-  //               </span>
-  //             </div>
-  //           </div>
-  //         </div>
+        <section className="w-full h-full mt-10 flex flex-col-reverse lg:flex-row justify-between items-center lg:items-start gap-x-8 gap-y-8">
+          <div className="w-full lg:w-2/3 h-full">
+            <h5 className="text-2xl font-bold text-primary-500">
+              Vidgo Premium Service RETENTION BONUS TRACKER
+            </h5>
 
-  //         <TeamCustomerTable teamCustomers={vidgoReport.teamCustomers} />
-  //       </div>
-  //     </section>
+            <div className="flex flex-col justify-start items-start">
+              <select
+                className="mt-4 py-1 px-4 select-clean"
+                value={dateSelected}
+                onChange={handleChange}
+              >
+                {dates.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    Month: {option.name}
+                  </option>
+                ))}
+              </select>
+            </div>
 
-  //     <section className='w-full h-full mt-10 flex flex-col-reverse lg:flex-row justify-between items-center lg:items-start gap-x-8 gap-y-8'>
-  //       <div className='w-full lg:w-2/3 h-full'>
-  //         <h5 className='text-2xl font-bold text-primary-500'>
-  //         Vidgo Premium Service RETENTION BONUS TRACKER
-  //         </h5>
+            <TableRetentionBonusTracker report={monthlyReport} />
+          </div>
 
-  //         <div className='flex flex-col justify-start items-start'>
-  //           <select className='mt-4 py-1 px-4 select-clean' value={dateSelected} onChange={handleChange}>
-  //             <option value='Month: May 2022'>Month: May 2022</option>
-  //           </select>
-  //         </div>
+          <div className="w-full lg:w-1/3">
+            <span className="text-xl font-bold">
+              Retention Bonus Benchmarked
+            </span>
+            <br />
 
-  //         <TableRetentionBonusTracker />
-  //       </div>
+            <div className="w-full h-full mt-5">
+              <BonusBenmarks />
 
-  //       <div className='w-full lg:w-1/3'>
-  //         <span className='text-xl font-bold'>Retention Bonus Benchmarked</span>
-  //         <br />
-
-  //         <div className='w-full h-full mt-5'>
-  //           <BonusBenmarks />
-
-  //           <RetentionMonthly />
-  //         </div>
-  //       </div>
-  //     </section>
-  //   </div>
-  // )
-}
+              <RetentionMonthly />
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  );
+};
 
 VidgoComisionsPage.getLayout = (page: ReactNode) => (
   <DashboardLayout>
     <Head>
       <title>{SEO.TITLE_PAGE} - Vidgo Comisions</title>
     </Head>
-
     {page}
   </DashboardLayout>
-)
+);
 
-export default VidgoComisionsPage
+export default VidgoComisionsPage;
