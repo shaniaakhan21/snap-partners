687e81f639924a5cce264be8c5f482e973e5f24b
gerardo15 <gerardo_bastidas@hotmail.com>
2022-08-10 09:33:59 +0200

    add agents to glosary

--- src/lib/utils/glosaryData.ts
+++ src/lib/utils/glosaryData.ts
@@ -32,6 +32,11 @@ export const glosaryData = {
       icon: '/static/glosary/merchant.png',
       title: 'Merchant',
       subtitle: 'Snap Merchant Control'
+    },
+    {
+      icon: '/static/glosary/agent.png',
+      title: 'Agent',
+      subtitle: 'Snap Financial Agent'
     }
   ],
 
@@ -40,7 +45,8 @@ export const glosaryData = {
       icons: [
         '/static/badges/FreeMemberDriver.png',
         '/static/badges/FreeMemberCustomer.png',
-        '/static/badges/FreeMemberMerchant.png'
+        '/static/badges/FreeMemberMerchant.png',
+        '/static/badges/FreeMemberAgent.png'
       ],
       title: 'Free Member',
       qualifications: '100% Free to sign up and refer merchants, drivers and customers.'
@@ -49,7 +55,8 @@ export const glosaryData = {
       icons: [
         '/static/badges/ManagerDriver.png',
         '/static/badges/ManagerCustomer.png',
-        '/static/badges/ManagerMerchant.png'
+        '/static/badges/ManagerMerchant.png',
+        '/static/badges/ManagerAgent.png'
       ],
       title: 'Manager',
       qualifications: 'Sign up through the Snap Delivered Website as a Manager for $299.99 Annually. Minimum $100 in personal commissionable volume to earn level.'
@@ -58,7 +65,8 @@ export const glosaryData = {
       icons: [
         '/static/badges/SupervisorDriver.png',
         '/static/badges/SupervisorCustomer.png',
-        '/static/badges/SupervisorMerchant.png'
+        '/static/badges/SupervisorMerchant.png',
+        '/static/badges/SupervisorAgent.png'
       ],
       title: 'Supervisor',
       qualifications: '3 PSM (Personally Sponsored Managers) All must be active. 500 Orders Per Month within organization. Limit of 250 Orders contributed from each leg. Must have 3 legs minimum with at least 50 Orders per leg. ($100 in Monthly Personal Commissionable Volume). Supervisors earn a 1% Open Line Override Below their 2nd Level down to the next Supervisor.'
@@ -67,7 +75,8 @@ export const glosaryData = {
       icons: [
         '/static/badges/DirectorDriver.png',
         '/static/badges/DirectorCustomer.png',
-        '/static/badges/DirectorMerchant.png'
+        '/static/badges/DirectorMerchant.png',
+        '/static/badges/DirectorAgent.png'
       ],
       title: 'Director',
       qualifications: '4 PSMs, All must be active. 5,000 Orders Per Month within organization. Limit of 2,500 Orders contributed from each leg. Must have 4 legs minimum with at least 250 Orders per leg. ($100 in Monthly Personal Commissionable Volume). Directors earn a 1% Open Line Override below their 3rd Level down to the next Qualified Director. Directors earn a 1% First Generational Override below their 1st Qualified Director’s organization in each leg.'
@@ -76,7 +85,8 @@ export const glosaryData = {
       icons: [
         '/static/badges/ExecutiveDriver.png',
         '/static/badges/ExecutiveCustomer.png',
-        '/static/badges/ExecutiveMerchant.png'
+        '/static/badges/ExecutiveMerchant.png',
+        '/static/badges/ExecutiveAgent.png'
       ],
       title: 'Excecutive',
       qualifications: '5 PSMs, All must be active. 10,000 Orders Per Month within organization. Limit of 5,000 Orders contributed from each leg. Must have 5 legs minimum with at least 500 Orders per leg. ($100 in Monthly Personal Commissionable Volume). Executives earn a 2% Open Line Override below their 5th Level down to the next Qualified Executive. Executives earn a 2% First Generational Override below their 1st Qualified Executive’s organization in each leg. Executives earn a 1% Second Generational Override below their 2nd Qualified Executive’s organization in each leg. 2% Profit Sharing Pool. 10 Shares for every 10,000 Orders. Limit of 5,000 Orders contributed from each leg.'
