1a623ba7dc9d64bff9784eca82c7d247487d778d
gerardo15 <gerardo_bastidas@hotmail.com>
2022-09-15 20:13:17 +0200

    remove upgrade to manager

--- src/components/page/signup/SignUpForm/FormByRole/utils/Steps/SuccessCode.tsx
+++ src/components/page/signup/SignUpForm/FormByRole/utils/Steps/SuccessCode.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 
-import { STEPS } from '.'
 import { IReferralLink } from 'lib/types'
 import { APP_INFO } from 'config/appInfo'
 import { IHandleStep } from '../types'
@@ -9,13 +8,90 @@ import { Button } from 'components/common/Button'
 import { CheckSuccess } from 'components/common/icons'
 import { MobileAppsLink } from 'components/common/mobileApps/MobileAppsLink'
 import { GTMTrack } from 'lib/utils/gtm'
+import { login } from '../../../../../../../lib/services/auth/login'
+import { handleFetchError } from '../../../../../../../lib/utils/handleFetchError'
+import { getUserMe } from '../../../../../../../lib/services/user/getUserMe'
+import { toast } from 'react-toastify'
+import { useRouter } from 'next/router'
+import { useRoleFromUrl } from '../../../../../../../lib/hooks/useRoleFromUrl'
+import { useAuthStore } from '../../../../../../../lib/stores'
+import { useState } from 'react'
+import { Spinner } from '../../../../../../common/loaders'
 
 const { SEO } = APP_INFO
 
 export const SuccessCode = ({ userTrack, handleStep, referralLink }: { userTrack: any, handleStep: IHandleStep, referralLink: IReferralLink }) => {
-  const handleClickContinue = () => {
-    GTMTrack.signUp(referralLink.role, 3)
-    handleStep(STEPS.UPGRADE_TO_MANAGER)
+  const router = useRouter()
+  const role = useRoleFromUrl()
+  const { auth, setAuth } = useAuthStore()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleClickLogin = async () => {
+    setIsLoading(true)
+
+    const { data: dataLogin, error: errorLogin } = await login({ username: userTrack.userInfo.username, password: userTrack.userInfo.password })
+
+    if (errorLogin) {
+      handleFetchError(errorLogin.status, errorLogin.info)
+      router.push('/auth/login')
+      setIsLoading(false)
+      return
+    }
+
+    const { data, error: errorUser } = await getUserMe({ token: dataLogin.token })
+
+    if (errorUser) {
+      handleFetchError(errorUser.status, errorUser.info)
+      setIsLoading(false)
+      return
+    }
+
+    toast('Login Successful!', { type: 'success' })
+    setIsLoading(false)
+    setAuth({
+      email: data.email,
+      name: data.name,
+      password: data.password,
+      phoneNumber: data.phoneNumber,
+      accessToken: dataLogin.token,
+      lastname: data.lastname,
+      roles: data.roles,
+      id: dataLogin.userId,
+      username: data.username,
+      referralCode: data.referralCode,
+      driver_status: data.driver_status,
+      idImage: data.idImage,
+      insuranceImage: data.insuranceImage,
+      isManager: data.ranks?.type === 'manager',
+      createdAt: data.createdAt,
+      ownerName: data.ownerName,
+      ranks: data.ranks,
+      updatedAt: data.updatedAt,
+      blocked: data.blocked,
+      deleted: data.deleted,
+      nsurAccount: {
+        nsurUserId: data.nsurUserId,
+        myPoints: auth?.nsurAccount?.myPoints || null
+      },
+      bank_information: data.bank_information
+    })
+    // When change auth state, directly the app push the user to /overview path
+    // This logic is on AuthPageLayout useEffect
+
+    return dataLogin.userId
+  }
+
+  if (isLoading) {
+    return (
+      <div className='flex justify-center items-center h-[85vh]'>
+        <Spinner classes='w-20 h-20 md:w-10 md:h-10' />
+      </div>
+    )
+  }
+
+  const handleSkip = () => {
+    GTMTrack.signUp(role, 4, null, 'no')
+    handleClickLogin()
   }
 
   return (
@@ -26,7 +102,7 @@ export const SuccessCode = ({ userTrack, handleStep, referralLink }: { userTrack
       <span className='text-xl text-primary-500'>Welcome!</span>
       <span className='text-4xl text-primary-500 font-bold'>{userTrack.userInfo.name ? userTrack.userInfo.name : userTrack.userInfo.merchant.name }</span>
 
-      <Button onClick={handleClickContinue} classes='w-full mt-10'>CONTINUE</Button>
+      <Button onClick={handleSkip} classes='w-full mt-10'>CONTINUE</Button>
       <br/>
       <span className='text-xl text-gray-500 text-center'>Please download the app below or visit our website at </span>
       <Link href={SEO.URL_PAGE}>
