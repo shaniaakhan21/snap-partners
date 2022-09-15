2bea82e930ae4081fc1d222034b885aead7d1850
gerardo15 <gerardo_bastidas@hotmail.com>
2022-09-15 20:42:51 +0200

    ibo only to agents

--- src/pages/marketing/index.tsx
+++ src/pages/marketing/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { useRef } from 'react'
 
 import type { Page, ReactNode } from 'lib/types'
+import { useAuthStore } from 'lib/stores'
 import { APP_INFO } from 'config/appInfo'
 
 import DashboardLayout from 'layouts/private/Dashboard'
@@ -12,27 +13,31 @@ import { ListMarketingTools } from 'components/page/marketing/CardTools/ListTool
 const { SEO } = APP_INFO
 
 const MarketingPage: Page = () => {
+  const { auth } = useAuthStore()
   const { current: marketingDetails } = useRef([
     {
       icon: <CustomerIcon />,
       title: 'Customer',
       subtitle: 'Subtitle text here',
       description: 'Select an image and share the link in the different social networks to get new Customers!',
-      to: '/marketing/customer'
+      to: '/marketing/customer',
+      snap: true
     },
     {
       icon: <DriverIcon />,
       title: 'Driver',
       subtitle: 'Subtitle text here',
       description: 'Select an image and share the link in the different social networks to get new Drivers On Board',
-      to: '/marketing/driver'
+      to: '/marketing/driver',
+      snap: true
     },
     {
       icon: <MerchantsIcon />,
       title: 'Merchant',
       subtitle: 'Subtitle text here',
       description: 'Select an image and share the link in the different social networks to get new Merchant Partners',
-      to: '/marketing/merchant'
+      to: '/marketing/merchant',
+      snap: true
     },
     {
       icon: <IBOIcon />,
@@ -55,16 +60,20 @@ const MarketingPage: Page = () => {
       </div>
 
       <ListMarketingTools>
-        {marketingDetails.map(detail => (
-          <MarketingTool
-            key={detail.title}
-            title={detail.title}
-            subtitle={detail.subtitle}
-            description={detail.description}
-            icon={detail.icon}
-            to={detail.to}
-          />
-        ))}
+        {marketingDetails.map(detail => {
+          const isSnap = (auth.roles.customer || auth.roles.driver || auth.roles.merchant)
+          if (detail.snap && !isSnap) return <></>
+          return (
+            <MarketingTool
+              key={detail.title}
+              title={detail.title}
+              subtitle={detail.subtitle}
+              description={detail.description}
+              icon={detail.icon}
+              to={detail.to}
+            />
+          )
+        })}
       </ListMarketingTools>
     </>
   )
