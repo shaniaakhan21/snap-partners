41e7880999f5d27eee752aedde6e25637d5863de
Gerardo Bastidas <gerardo_bastidas@hotmail.com>
2022-11-19 18:59:18 +0100

    update link

--- src/pages/overview.tsx
+++ src/pages/overview.tsx
@@ -47,7 +47,7 @@ const DashboardOverViewPage: Page = () => {
       </>
       <>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mb-5'>
-          <a href="https://jorns.smartstudenthub.com/Account/ERCLogin" target={'_blank'}>
+          <a href="https://jorns.smartstudenthub.com/ERC/Register" target={'_blank'}>
             <img src='/images/1.png' alt='Agent logo' />
           </a>
           <a href="https://snapfinancialcertified.com/" target={'_blank'}>
