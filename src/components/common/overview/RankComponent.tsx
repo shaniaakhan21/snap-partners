14355d1df75e3f623f19dff1be91f427ca635b50
Gerardo Bastidas <gerardo_bastidas@hotmail.com>
2023-04-20 08:08:33 +0200

    api connection for rank

--- src/components/common/overview/RankComponent.tsx
+++ src/components/common/overview/RankComponent.tsx
@@ -7,6 +7,8 @@ import StarIcon from '@material-ui/icons/Star'
 import { styled } from '@material-ui/core/styles'
 import BarWithText from './BarWithText'
 import { data } from './mock'
+import axios from 'axios'
+import { getLocalStorage } from 'lib/utils/localStorage'
 
 interface TabPanelProps {
   children?: ReactNode;
@@ -60,22 +62,22 @@ interface GV {
   percentage: number
 }
 
+interface PVC {
+  value: number,
+  percentage: number
+}
+
 interface Post {
+  pvc: PVC
   commissionVol: number,
   PSMRatio: string,
   PSMPercentage: number,
   workingLegs: WorkingLegs,
   gv: GV
 }
-interface PVC {
-  value: number,
-  percentage: number
-}
+
 
 interface RankData {
-  fm: {
-    pvc: PVC
-  },
   mng: Post,
   sv: Post,
   dct: Post,
@@ -88,8 +90,15 @@ export default function RankComponent () {
   const [rankData, setRankData] = useState<RankData>(null)
 
   useEffect(() => {
-    const details = data as RankData
-    setRankData(details)
+    (async () => {
+      const token = getLocalStorage('accessToken')
+      const response = await axios.get('/api/reports/getRanks', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      setRankData(response.data)
+    })()
   }, [])
 
   const handleChange = (event: SyntheticEvent, newValue: number) => {
@@ -114,14 +123,6 @@ export default function RankComponent () {
             </StyledBox>
             <h1 className="text-2xl text-black font-bold pl-2">Free Member</h1>
           </div>
-          <div className='col-span-3 text-xs pt-5'>
-            <span className="text-15">To become a <strong>Manager</strong> you'll need</span>
-          </div>
-          <div className="flex pt-2">
-            <span className="text-left text-10"><strong>{`You have ${rankData.fm.pvc.value} PVC`}</strong></span>
-            <span className="text-right ml-auto text-10"><strong>You need 500 PVC</strong></span>
-          </div>
-          <BarWithText progressColor={'#DD4C37'} value={+rankData.fm.pvc.percentage} variant={'determinate'}/>
           </>
         }
       </TabPanel>
@@ -135,42 +136,11 @@ export default function RankComponent () {
             <h1 className="text-2xl text-black font-bold pl-2">Manager</h1>
           </div>
           <div className='col-span-3 text-xs pt-5'>
+            <span className="text-15">To be a <strong>Manager</strong> you need</span>
+          </div>          
+          <div className='col-span-3 text-xs pt-5'>
             <span className='text-10'><strong>You need 100 Personal Commissionable Volume</strong></span>
             <BarWithText progressColor={'#C99FFF'} value={+rankData.mng.commissionVol} variant={'determinate'} />
-          </div>
-          <div className='col-span-3 text-xs pt-5'>
-            <span className="text-15">To become a <strong>Supervisor</strong> you'll need</span>
-          </div>
-          <div className="flex pt-2">
-            <span className="text-left text-10"><strong>{`You have ${rankData.mng.PSMRatio} PSM`}</strong></span>
-            <span className="text-right ml-auto text-10"><strong>You need 3 Active PSM</strong></span>
-          </div>
-          <BarWithText progressColor={'#C99FFF'} value={+rankData.mng.PSMPercentage} variant={'determinate'}/>
-          <div className="flex pt-2">
-            <span className="text-left text-10"><strong>You need</strong></span>
-            <span className="text-right ml-auto text-10"><strong>3 Working Legs, 2500 V. Each</strong></span>
-          </div>
-          <div>
-            <div className='grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 gap-2'>
-              {
-                rankData.mng.workingLegs && Object.values(rankData.mng.workingLegs).map((leg, index) => (
-                  <div className='col-span-1'>
-                    {<BarWithText progressColor={'#C99FFF'} value={leg.percentage} variant={'determinate'}/>}
-                  </div>
-                ))
-              }
-
-              <div className='col-span-1 flex justify-end items-end'>
-                <button className="flex items-center bg-red-600 hover:bg-red-700 text-white font-bold h-6 w-10 py-2 px-4 rounded-l-full rounded-r-full">+</button>
-              </div>
-            </div>
-          </div>
-          <div className="flex pt-2">
-            <span className="text-left text-10"><strong>{`Gv You have $${rankData.mng.gv.value}`}</strong></span>
-            <span className="text-right ml-auto text-10"><strong>You need $5000</strong></span>
-          </div>
-          <div className='col-span-1'>
-            <BarWithText progressColor={'#C99FFF'} value={+rankData.mng.gv.percentage} variant={'determinate'}/>
           </div>
           </>
         }
@@ -186,12 +156,13 @@ export default function RankComponent () {
             <h1 className="text-2xl text-black font-bold pl-2">Supervisor</h1>
           </div>
           <div className='col-span-3 text-xs pt-5'>
+            <span className="text-15">To be a <strong>Supervisor</strong> you need</span>
+          </div>
+          <div className='col-span-3 text-xs pt-5'>
             <span className='text-10'><strong>You need 100 Personal Commissionable Volume</strong></span>
             <BarWithText progressColor={'#54A52C'} value={+rankData.sv.commissionVol} variant={'determinate'} />
           </div>
-          <div className='col-span-3 text-xs pt-5'>
-            <span className="text-15">To become a <strong>Supervisor</strong> you'll need</span>
-          </div>
+
           <div className="flex pt-2">
             <span className="text-left text-10"><strong>{`You have ${rankData.sv.PSMRatio} PSM`}</strong></span>
             <span className="text-right ml-auto text-10"><strong>You need 3 Active PSM</strong></span>
@@ -236,11 +207,11 @@ export default function RankComponent () {
             <h1 className="text-2xl text-black font-bold pl-2">Director</h1>
           </div>
           <div className='col-span-3 text-xs pt-5'>
+            <span className="text-15">To be a <strong>Director</strong> you need</span>
+          </div>          
+          <div className='col-span-3 text-xs pt-5'>
             <span className='text-10'><strong>You need 100 Personal Commissionable Volume</strong></span>
             <BarWithText progressColor={'#F18A00'} value={+rankData.dct.commissionVol} variant={'determinate'} />
-          </div>
-          <div className='col-span-3 text-xs pt-5'>
-            <span className="text-15">To become a <strong>Supervisor</strong> you'll need</span>
           </div>
           <div className="flex pt-2">
             <span className="text-left text-10"><strong>{`You have ${rankData.dct.PSMRatio} PSM`}</strong></span>
@@ -268,7 +239,7 @@ export default function RankComponent () {
           </div>
           <div className="flex pt-2">
             <span className="text-left text-10"><strong>{`Gv You have $${rankData.dct.gv.value}`}</strong></span>
-            <span className="text-right ml-auto text-10"><strong>You need $5000</strong></span>
+            <span className="text-right ml-auto text-10"><strong>You need $50000</strong></span>
           </div>
           <div className='col-span-1'>
             <BarWithText progressColor={'#F18A00'} value={+rankData.dct.gv.percentage} variant={'determinate'}/>
@@ -286,20 +257,20 @@ export default function RankComponent () {
             <h1 className="text-2xl text-black font-bold pl-2">Executive</h1>
           </div>
           <div className='col-span-3 text-xs pt-5'>
+            <span className="text-15">To be a <strong>Executive</strong> you need</span>
+          </div>          
+          <div className='col-span-3 text-xs pt-5'>
             <span className='text-10'><strong>You need 100 Personal Commissionable Volume</strong></span>
             <BarWithText progressColor={'#000000'} value={+rankData.exec.commissionVol} variant={'determinate'} />
           </div>
-          <div className='col-span-3 text-xs pt-5'>
-            <span className="text-15">To become a <strong>Supervisor</strong> you'll need</span>
-          </div>
           <div className="flex pt-2">
             <span className="text-left text-10"><strong>{`You have ${rankData.exec.PSMRatio} PSM`}</strong></span>
-            <span className="text-right ml-auto text-10"><strong>You need 3 Active PSM</strong></span>
+            <span className="text-right ml-auto text-10"><strong>You need 5 Active PSM</strong></span>
           </div>
           <BarWithText progressColor={'#000000'} value={+rankData.exec.PSMPercentage} variant={'determinate'}/>
           <div className="flex pt-2">
             <span className="text-left text-10"><strong>You need</strong></span>
-            <span className="text-right ml-auto text-10"><strong>3 Working Legs, 2500 V. Each</strong></span>
+            <span className="text-right ml-auto text-10"><strong>5 Working Legs, 2500 V. Each</strong></span>
           </div>
           <div>
             <div className='grid grid-cols-2 sm:grid-cols-6 md:grid-cols-6 gap-2'>
@@ -318,7 +289,7 @@ export default function RankComponent () {
           </div>
           <div className="flex pt-2">
             <span className="text-left text-10"><strong>{`Gv You have $${rankData.exec.gv.value}`}</strong></span>
-            <span className="text-right ml-auto text-10"><strong>You need $5000</strong></span>
+            <span className="text-right ml-auto text-10"><strong>You need $100000</strong></span>
           </div>
           <div className='col-span-1'>
             <BarWithText progressColor={'#000000'} value={+rankData.exec.gv.percentage} variant={'determinate'}/>
