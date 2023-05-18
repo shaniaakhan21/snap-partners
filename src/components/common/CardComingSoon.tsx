7473c9bef3fd52d90d5b7459c418d6c75fae6128
Javid Haji-zada <javid@Javids-MacBook-Pro.local>
2023-05-18 13:29:47 +0400

    Added ComingSoon in Compensation Plan

--- src/components/common/CardComingSoon.tsx
+++ src/components/common/CardComingSoon.tsx
@@ -1,10 +1,10 @@
-export const CardComingSoon = () => {
+export const CardComingSoon = ({ text = '' }) => {
   return (
     <div className='px-5 py-12 border-4 border-solid border-red-500 max-w-2xl rounded-md text-center bg-white flex flex-col justify-center items-center'>
       <img src='/images/logo.svg' className='w-36 h-36' />
       <span className='text-primary-500 text-4xl font-black'>COMING SOON</span>
       <br />
-      <p>We are almost there to introduce our new application to the world, in the meantime, you can follow us on social networks to get the latest updates.</p>
+      <p>{text || 'We are almost there to introduce our new application to the world, in the meantime, you can follow us on social networks to get the latest updates.'}</p>
     </div>
   )
 }
