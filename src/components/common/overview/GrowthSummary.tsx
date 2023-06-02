87ff77bcb5ac3716624227bc266d72935a557a40
Hashan Chathuranga <chathuranga.dyrani@gmail.com>
2023-06-02 16:29:11 +0400

    Growth Summary Responsive Design

--- src/components/common/overview/GrowthSummary.tsx
+++ src/components/common/overview/GrowthSummary.tsx
@@ -8,6 +8,7 @@ import { KeyboardArrowDown } from '@mui/icons-material'
 
 interface DownLineRow {
     name: string;
+    nameShort: string;
     direct: number;
     openLineManager: number;
     openLineDirector: number;
@@ -16,11 +17,11 @@ interface DownLineRow {
 }
 
 const downLineData: DownLineRow[] = [
-  { name: 'ERC', direct: 58, openLineManager: 51, openLineDirector: 59, openLineSupervisory: 18, openLineExecutive: 20 },
-  { name: 'Delivery Clients', direct: 82, openLineManager: 46, openLineDirector: 17, openLineSupervisory: 48, openLineExecutive: 9 },
-  { name: 'Delivery Rest.', direct: 76, openLineManager: 7, openLineDirector: 10, openLineSupervisory: 47, openLineExecutive: 34 },
-  { name: 'Delivery Drivers', direct: 97, openLineManager: 13, openLineDirector: 46, openLineSupervisory: 33, openLineExecutive: 54 },
-  { name: 'Vidgo', direct: 4, openLineManager: 43, openLineDirector: 98, openLineSupervisory: 50, openLineExecutive: 98 }
+  { name: 'ERC', nameShort: 'ERC', direct: 58, openLineManager: 51, openLineDirector: 59, openLineSupervisory: 18, openLineExecutive: 20 },
+  { name: 'Delivery Clients', nameShort: 'D. Clients', direct: 82, openLineManager: 46, openLineDirector: 17, openLineSupervisory: 48, openLineExecutive: 9 },
+  { name: 'Delivery Rest.', nameShort: 'D. Rest.', direct: 76, openLineManager: 7, openLineDirector: 10, openLineSupervisory: 47, openLineExecutive: 34 },
+  { name: 'Delivery Drivers', nameShort: 'D. Drivers', direct: 97, openLineManager: 13, openLineDirector: 46, openLineSupervisory: 33, openLineExecutive: 54 },
+  { name: 'Vidgo', nameShort: 'Vidgo', direct: 4, openLineManager: 43, openLineDirector: 98, openLineSupervisory: 50, openLineExecutive: 98 }
 ]
 
 const tabStyle = {
@@ -55,25 +56,27 @@ export default function GrowthSummary () {
         }}>
         {['Downline', 'New Clients', 'Orders', 'Volume'].map((label, index) => (<Tab sx={tabStyle} color={index === selectedTab ? '#ffffff' : undefined} label={label} />))}
       </Tabs>
-      <div className="my-4 flex items-center">
+      <div className="my-4 md:flex items-center">
         <h5 className="text-xl text-black font-bold flex-1">New IBOs</h5>
-        <span className="text-sm text-gray-600 mr-3">Month</span>
-        <Select
-          indicator={<KeyboardArrowDown />}
-          sx={{
-            width: 180,
-            border: 'none',
-            [`& .${selectClasses.indicator}`]: {
-              transition: '0.2s',
-              [`&.${selectClasses.expanded}`]: {
-                transform: 'rotate(-180deg)'
+        <div className='flex items-center justify-end'>
+          <span className="text-sm text-gray-600 mr-3">Month</span>
+          <Select
+            indicator={<KeyboardArrowDown />}
+            sx={{
+              width: 180,
+              border: 'none',
+              [`& .${selectClasses.indicator}`]: {
+                transition: '0.2s',
+                [`&.${selectClasses.expanded}`]: {
+                  transform: 'rotate(-180deg)'
+                }
               }
-            }
-          }}
-          value={''}
-        >
-          <Option value="">Current Month</Option>
-        </Select>
+            }}
+            value={''}
+          >
+            <Option value="">Current Month</Option>
+          </Select>
+        </div>
       </div>
       <div>
         <table className="mt-4 w-full text-center">
@@ -81,10 +84,14 @@ export default function GrowthSummary () {
             <tr>
               <th className='text-xs font-normal'>&nbsp;</th>
               <th className='text-xs font-normal'>Direct</th>
-              <th className='text-xs font-normal'>Open-Line Manager</th>
-              <th className='text-xs font-normal'>Open-Line Director</th>
-              <th className='text-xs font-normal'>Open-Line Supervisory</th>
-              <th className='text-xs font-normal'>Open-Line Executive</th>
+              <th className='text-xs font-normal hidden xs:block'>Open-Line Manager</th>
+              <th className='text-xs font-normal xs:hidden'>O-L Mngr.</th>
+              <th className='text-xs font-normal hidden xs:block'>Open-Line Director</th>
+              <th className='text-xs font-normal xs:hidden'>O-L Dire.</th>
+              <th className='text-xs font-normal hidden xs:block'>Open-Line Supervisory</th>
+              <th className='text-xs font-normal xs:hidden'>O-L Supv.</th>
+              <th className='text-xs font-normal hidden xs:block'>Open-Line Executive</th>
+              <th className='text-xs font-normal xs:hidden'>O-L Exec.</th>
               <th className='text-xs font-normal'>Total</th>
             </tr>
           </thead>
@@ -92,7 +99,8 @@ export default function GrowthSummary () {
             {downLineData.map((row, idx) => {
               return (
                 <tr key={idx} className='h-10'>
-                  <td className='text-xs font-normal'>{row.name}</td>
+                  <td className='text-xs font-normal hidden xs:block'>{row.name}</td>
+                  <td className='text-xs font-normal xs:hidden'>{row.nameShort}</td>
                   <td className='text-sm text-gray-500'>{row.direct}</td>
                   <td className='text-sm text-gray-500'>{row.openLineManager}</td>
                   <td className='text-sm text-gray-500'>{row.openLineDirector}</td>
@@ -100,8 +108,8 @@ export default function GrowthSummary () {
                   <td className='text-sm text-gray-500'>{row.openLineExecutive}</td>
                   <td className='text-sm text-gray-500'>
                     <div className=' flex items-center justify-between'>
-                      {Object.values(row).slice(1).reduce((a, r) => a + r, 0)}
-                      <i className="fa-sharp fa-solid fa-square-plus fa-1.5x text-primary-500"></i>
+                      {Object.values(row).slice(2).reduce((a, r) => a + r, 0)}
+                      <i className="fa-sharp fa-solid fa-square-plus fa-1.5x text-primary-500 ml-2"></i>
                     </div>
                   </td>
                 </tr>
