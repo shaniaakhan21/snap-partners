268cd04248ea24cfa4aa6d20b8ce6dc7004147a4
Gerardo Bastidas <gerardo_bastidas@hotmail.com>
2023-06-09 21:45:57 +0200

    add customers

--- src/components/common/dashBackOffice/MonthlyCustomerTracking.tsx
+++ src/components/common/dashBackOffice/MonthlyCustomerTracking.tsx
@@ -1,25 +1,56 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useAuthStore } from 'lib/stores'
+import { GeneralModal } from 'components/page/genealogy/OldGenealogy/Modals/GeneralModal'
+import { DataGrid } from '@mui/x-data-grid'
 
 interface MonthlyMilestonesData {
   revenue: number
   customerCount: number
 }
 
+const Table = (props) => {
+  const rows = useMemo(() => props.rows, [props.rows])
+  const columns = useMemo(() => props.columns, [props.columns])
+  return (
+    <DataGrid
+      rows={rows}
+      autoHeight
+      columns={columns}
+      density="compact"
+      initialState={{
+        sorting: {
+          sortModel: props.sortModel
+        },
+        pagination: { paginationModel: { pageSize: 10 } }
+      }}
+    />
+  )
+}
+
 export default function MonthlyMilestones () {
   const { auth } = useAuthStore()
   const [data, setData] = useState<MonthlyMilestonesData>()
+  const [open, setOpen] = useState(false)
+  const [rows, setRows] = useState([])
 
   useEffect(() => {
-    fetch(`/api/ibo/customer/tracking`, {
+    fetch('/api/ibo/customer/tracking', {
       method: 'GET',
       headers: { Authorization: `Bearer ${auth.accessToken}` }
     }).then((response) => {
       response.json().then((data) => {
         setData(data.data)
+        setRows(data.data.customers)
       })
     })
   }, [])
+
+  const columns = [
+    { field: 'id', headerName: 'User Id', minWidth: 130, flex: 1 },
+    { field: 'name', headerName: 'Name', minWidth: 130, flex: 1 },
+    { field: 'revenue', headerName: 'Revenue', minWidth: 130, flex: 1 }
+  ]
+
   return (
     <>
       <div className="w-full max-w-full p-4 space-y-2 bg-white rounded-xl">
@@ -42,12 +73,18 @@ export default function MonthlyMilestones () {
             </div>
           </div>
         </div>
-        <button className="rounded-full bg-primary-500 w-full flex flex-row items-center justify-center bg-gray-300 text-gray-500 cursor-not-allowed" disabled>
+        <button onClick={() => { setOpen(true) }} style={{ cursor: 'pointer' }} className="rounded-full bg-primary-500 w-full flex flex-row items-center justify-center bg-red-500 text-gray-500">
           <img className='w-1/14' src='/images/icons/order-details.svg' alt="Current profile photo" />
-          <p className='text-xs text-white font-medium p-2 uppercase'>View Orders (Coming Soon)</p>
+          <p className='text-xs text-white font-medium p-2 uppercase'>View Customers</p>
         </button>
       </div>
 
+      <GeneralModal onClose={() => { setOpen(false) }} open={open} showClose={true}>
+        <div style={{ marginTop: 50 }}>
+          <h1 style={{ marginBottom: 20 }}>Monthly Customer Tracking List</h1>
+          <Table columns={columns} rows={rows} sortModel={[{ field: 'name', sort: 'asc' }]} />
+        </div>
+      </GeneralModal>
     </>
   )
 }
