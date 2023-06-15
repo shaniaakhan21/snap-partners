003f99457bf9d626954b68723781ed253c4c5665
amir ali <amiralirang98@gmail.com>
2023-06-15 18:45:30 +0530

    changes made to pdf viewer to fix textLayer issue

--- src/components/page/compensation-plan/PDFViewer.tsx
+++ src/components/page/compensation-plan/PDFViewer.tsx
@@ -1,18 +1,37 @@
-import { Viewer, Worker, SpecialZoomLevel, PageChangeEvent } from '@react-pdf-viewer/core'
+// import { Viewer, Worker, SpecialZoomLevel, PageChangeEvent } from '@react-pdf-viewer/core'
+import { Document, Page, pdfjs } from 'react-pdf/dist/esm/entry.webpack'
 import { GTMTrack } from 'lib/utils/gtm'
+import { useState } from 'react'
 
 export default function PDFViewer () {
-  const onPageChange = (e: PageChangeEvent) => {
-    GTMTrack.changeCompensationPlanPage(e.currentPage)
+  const onPageChange = (e) => {
+     GTMTrack.changeCompensationPlanPage(numPages)
   }
+  const [numPages, setNumPages] = useState(null)
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages)
+  }
+  GTMTrack.changeCompensationPlanPage(numPages)
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.12.313/build/pdf.worker.js">
-      <div
-        className='mx-auto w-full'
-        style={{ height: '900px', maxWidth: 1280 }}
+    <>
+      <Document
+        file='/static/FULL_Comp_Plan_1.pdf'
+        onLoadSuccess={onDocumentLoadSuccess}
       >
-        <Viewer fileUrl="/static/FULL_Comp_Plan_1.pdf" defaultScale={SpecialZoomLevel.PageFit} onPageChange={onPageChange}/>
-      </div>
-    </Worker>
+        {/* <Page pageNumber={pageNumber} /> */}
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+        ))}
+      </Document>
+    </>
+    // <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.12.313/build/pdf.worker.js">
+    //   <div
+    //     className='mx-auto w-full'
+    //     style={{ height: '900px', maxWidth: 1280 }}
+    //   >
+    //     <Viewer fileUrl="/static/FULL_Comp_Plan_1.pdf" defaultScale={SpecialZoomLevel.PageFit} onPageChange={onPageChange}/>
+    //   </div>
+    // </Worker>
   )
 }
