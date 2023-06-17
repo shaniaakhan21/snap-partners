16f67a55328b6947c26dab3672c0b7a3a38dbbed
Gerardo Bastidas <gerardo_bastidas@hotmail.com>
2023-06-17 21:52:07 +0200

    binary

--- src/pages/binarytree.tsx
+++ src/pages/binarytree.tsx
@@ -198,9 +198,20 @@ const ComingSoon: PageNext = () => {
             }} className="oc-view" style={{ position: 'absolute', right: 5 }}><i className="fa-solid fa-eye"></i></div>
 
           )}
+          {!['Blocked', 'Available'].includes(nodeData.name) && (
+            <div className="oc-view" style={{ position: 'relative' }}>{nodeData.PV}</div>
+          )}
           {nodeData.title}
         </div>
-        <div className="oc-content">{nodeData.subtitle}</div>
+        <div className="oc-content">
+        {!['Blocked', 'Available'].includes(nodeData.name) && (
+            <span>{nodeData.LL}</span>
+          )}
+        <br/>
+        {!['Blocked', 'Available'].includes(nodeData.name) && (
+            <span>{nodeData.RL}</span>
+          )}
+        </div>
       </div>
     )
   }
