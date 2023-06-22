150b6b6bf6965f8a35d5c28f18d52aab1298d307
Hashan Chathuranga <chathuranga.dyrani@gmail.com>
2023-06-22 16:42:34 +0400

    Growth Summary Backend & Integration

--- src/components/common/overview/GrowthSummary.tsx
+++ src/components/common/overview/GrowthSummary.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
+import {useMemo, useState} from 'react'
 import { Tab, Tabs } from '@mui/material'
 import BarWithText from './BarWithText'
 import { Button } from '../Button'
 import HiddenTabScrollButton from './HiddenTabScrollButton'
 import { Select, Option, selectClasses } from '@mui/joy'
 import { KeyboardArrowDown } from '@mui/icons-material'
+import * as React from "react";
+import {getLocalStorage} from "../../../lib/utils/localStorage";
+import axios from "axios";
 
 interface DownLineRow {
     name: string;
@@ -35,8 +38,138 @@ const tabStyle = {
   }
 }
 
+type ClientDataItem = {
+  admin?: number,
+  customer?: number,
+  agent?: number,
+  driver?: number,
+  merchant?: number,
+  integrousAssociate?: number,
+  integrousCustomer?: number,
+}
+
+type OrdersDataItem = {
+  asCustomer?: number,
+  asDriver?: number,
+  asRestaurant?: number,
+  asErc?: number,
+}
+
+type GrowthSummaryData = {
+  volume: { direct?: number, manager?: number, director?: number, supervisor?: number, executive?: number },
+  newIOBs: { direct?: number, manager?: number, director?: number, supervisor?: number, executive?: number },
+  newClientsData: { direct: ClientDataItem, manager: ClientDataItem, director: ClientDataItem, supervisor: ClientDataItem, executive: ClientDataItem },
+  newOrdersData: { direct: OrdersDataItem, manager: OrdersDataItem, director: OrdersDataItem, supervisor: OrdersDataItem, executive: OrdersDataItem },
+}
+
+const sum = (...args: (number | undefined)[]) => args.reduce((a, b) => a + (b ?? 0), 0)
+
 export default function GrowthSummary () {
+
+  const [monthSelected, setMonthSelected] = React.useState(new Date().getMonth()) // 0-11
+  const [yearSelected, setYearSelected] = React.useState(new Date().getFullYear())
+
+  const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+  const years = []
+  for (let i = new Date().getFullYear(); i >= 2022; i--) {
+    years.push(i)
+  }
+
+  const [growthSummaryData, setGrowthSummaryData] = React.useState<GrowthSummaryData | undefined>()
   const [selectedTab, setSelectedTab] = useState(0)
+
+  React.useEffect(() => {
+    (async () => {
+      const token = getLocalStorage('accessToken')
+      const response = await axios.get('/api/reports/getGrowthSummary', {
+        params: {
+          monthSelected,
+          yearSelected
+        },
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      console.log(response.data)
+      setGrowthSummaryData(response.data)
+    })()
+  }, [monthSelected, yearSelected])
+
+  const showingData = useMemo(() => {
+    if (!growthSummaryData) return []
+    const { newIOBs, newClientsData, newOrdersData, volume } = growthSummaryData
+    switch (selectedTab) {
+      case 0:
+        return [
+          [
+            'Total',
+            newIOBs?.direct ?? 0,
+            newIOBs?.manager ?? 0,
+            newIOBs?.director ?? 0,
+            newIOBs?.supervisor ?? 0,
+            newIOBs?.executive ?? 0,
+            sum(
+              newIOBs?.direct,
+              newIOBs?.manager,
+              newIOBs?.director,
+              newIOBs?.supervisor,
+              newIOBs?.executive
+            )
+          ]
+        ]
+      case 1:
+        return [['ERC', 'agent'], ['Delivery Clients', 'customer'], ['Delivery Drivers', 'driver'], ['Delivery Rest.', 'merchant']].map(([name, type]) =>           [
+          name,
+          newClientsData?.direct?.[type] ?? 0,
+          newClientsData?.manager?.[type] ?? 0,
+          newClientsData?.director?.[type] ?? 0,
+          newClientsData?.supervisor?.[type] ?? 0,
+          newClientsData?.executive?.[type] ?? 0,
+          sum(
+            newClientsData?.direct?.[type],
+            newClientsData?.manager?.[type],
+            newClientsData?.director?.[type],
+            newClientsData?.supervisor?.[type],
+            newClientsData?.executive?.[type]
+          )
+        ])
+      case 2:
+        return [['ERC', 'asErc'], ['Delivery Clients', 'asCustomer'], ['Delivery Drivers', 'asDriver'], ['Delivery Rest.', 'asRestaurant']].map(([name, type]) =>           [
+          name,
+          newOrdersData?.direct?.[type] ?? 0,
+          newOrdersData?.manager?.[type] ?? 0,
+          newOrdersData?.director?.[type] ?? 0,
+          newOrdersData?.supervisor?.[type] ?? 0,
+          newOrdersData?.executive?.[type] ?? 0,
+          sum(
+            newOrdersData?.direct?.[type],
+            newOrdersData?.manager?.[type],
+            newOrdersData?.director?.[type],
+            newOrdersData?.supervisor?.[type],
+            newOrdersData?.executive?.[type]
+          )
+        ])
+      case 3:
+        return [
+          [
+            "",
+            volume?.manager ?? 0,
+            volume?.director ?? 0,
+            volume?.supervisor ?? 0,
+            volume?.executive ?? 0,
+            sum(
+              volume?.manager,
+              volume?.director,
+              volume?.supervisor,
+              volume?.executive
+            )
+          ]
+        ]
+      default:
+        return []
+    }
+  }, [selectedTab, growthSummaryData])
 
   return (
     <div className="bg-white rounded-lg px-2.5 py-3">
@@ -57,25 +190,40 @@ export default function GrowthSummary () {
         {['Downline', 'New Clients', 'Orders', 'Volume'].map((label, index) => (<Tab sx={tabStyle} color={index === selectedTab ? '#ffffff' : undefined} label={label} />))}
       </Tabs>
       <div className="my-4 md:flex items-center">
-        <h5 className="text-xl text-black font-bold flex-1">New IBOs</h5>
-        <div className='flex items-center justify-end'>
-          <span className="text-sm text-gray-600 mr-3">Month</span>
-          <Select
-            indicator={<KeyboardArrowDown />}
-            sx={{
-              width: 180,
-              border: 'none',
-              [`& .${selectClasses.indicator}`]: {
-                transition: '0.2s',
-                [`&.${selectClasses.expanded}`]: {
-                  transform: 'rotate(-180deg)'
-                }
-              }
-            }}
-            value={''}
+        <h5 className="text-xl text-black font-bold flex-1">{['New IBOs', 'New Clients', 'New Orders', 'New Orders Volume'][selectedTab]}</h5>
+        <div className='flex flex-row items-center'>
+          <select
+            id='legalType'
+            name='legalType'
+            className='cursor-pointer outline-none appearance-none rounded-md border border-solid border-black py-2 pl-2 pr-12 text-sm sm:text-base'
+            placeholder='User Rank'
+            onChange={(current) => { setYearSelected(parseInt(current.target.value)) }}
           >
-            <Option value="">Current Month</Option>
-          </Select>
+            {years.map((y, i) => {
+              return (
+                <option key={i} selected={(new Date().getFullYear() === y)} value={y}>
+                  {y}
+                </option>
+              )
+            })
+            }
+          </select>
+          <select
+            id='legalType'
+            name='legalType'
+            className='ml-5 cursor-pointer outline-none appearance-none rounded-md border border-solid border-black py-2 pl-2 text-sm sm:text-base'
+            placeholder='User Rank'
+            onChange={(current) => { setMonthSelected(parseInt(current.target.value)) }}
+          >
+            {month.map((m, i) => {
+              return (
+                <option key={i} selected={(new Date().getMonth() === i)} value={i}>
+                  {m}
+                </option>
+              )
+            })
+            }
+          </select>
         </div>
       </div>
       <div>
@@ -83,35 +231,30 @@ export default function GrowthSummary () {
           <thead>
             <tr>
               <th className='text-xs font-normal'>&nbsp;</th>
-              <th className='text-xs font-normal'>Direct</th>
-              <th className='text-xs font-normal hidden xs:block'>Open-Line Manager</th>
+              {selectedTab !== 3 && <th className='text-xs font-normal'>Direct</th>}
+              <th className='text-xs font-normal hidden xs:table-cell'>Open-Line Manager</th>
               <th className='text-xs font-normal xs:hidden'>O-L Mngr.</th>
-              <th className='text-xs font-normal hidden xs:block'>Open-Line Director</th>
+              <th className='text-xs font-normal hidden xs:table-cell'>Open-Line Director</th>
               <th className='text-xs font-normal xs:hidden'>O-L Dire.</th>
-              <th className='text-xs font-normal hidden xs:block'>Open-Line Supervisory</th>
+              <th className='text-xs font-normal hidden xs:table-cell'>Open-Line Supervisory</th>
               <th className='text-xs font-normal xs:hidden'>O-L Supv.</th>
-              <th className='text-xs font-normal hidden xs:block'>Open-Line Executive</th>
+              <th className='text-xs font-normal hidden xs:table-cell'>Open-Line Executive</th>
               <th className='text-xs font-normal xs:hidden'>O-L Exec.</th>
               <th className='text-xs font-normal'>Total</th>
             </tr>
           </thead>
           <tbody>
-            {downLineData.map((row, idx) => {
+            {showingData.map((row, idx) => {
               return (
                 <tr key={idx} className='h-10'>
-                  <td className='text-xs font-normal hidden xs:block'>{row.name}</td>
-                  <td className='text-xs font-normal xs:hidden'>{row.nameShort}</td>
-                  <td className='text-sm text-gray-500'>{row.direct}</td>
-                  <td className='text-sm text-gray-500'>{row.openLineManager}</td>
-                  <td className='text-sm text-gray-500'>{row.openLineDirector}</td>
-                  <td className='text-sm text-gray-500'>{row.openLineSupervisory}</td>
-                  <td className='text-sm text-gray-500'>{row.openLineExecutive}</td>
-                  <td className='text-sm text-gray-500'>
-                    <div className=' flex items-center justify-between'>
-                      {Object.values(row).slice(2).reduce((a, r) => a + r, 0)}
-                      <i className="fa-sharp fa-solid fa-square-plus fa-1.5x text-primary-500 ml-2"></i>
-                    </div>
-                  </td>
+                  <td className='text-xs font-normal'>{row[0]}</td>
+                  {row.slice(1).map((r, idx2) => <td key={`${idx}_${idx2}`} className='text-sm text-gray-500'>{r}</td>)}
+                  {/*<td className='text-sm text-gray-500'>*/}
+                  {/*  <div className=' flex items-center justify-between'>*/}
+                  {/*    {Object.values(row).slice(2).reduce((a, r) => a + r, 0)}*/}
+                  {/*    <i className="fa-sharp fa-solid fa-square-plus fa-1.5x text-primary-500 ml-2"></i>*/}
+                  {/*  </div>*/}
+                  {/*</td>*/}
                 </tr>
               )
             })}
