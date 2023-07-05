e927128ca6d90d83789177313ee69a60a942fba5
thathsara <thathsararaviraj>
2023-07-05 14:25:57 +0400

    add safe validation for percentatge

--- src/components/common/dashBackOffice/RankTracker.tsx
+++ src/components/common/dashBackOffice/RankTracker.tsx
@@ -66,11 +66,14 @@ const calculateCurrentRank = (currentLeftTot: number, currentRightTot: number, i
 
 const calculatePercentage = (adjustedTotLL: number, powerLeg: number, adjustedTotRL: number,
   nonPowerLeg: number, adjustedPV: number, adjustedActiveLeft: number, adjustedActiveRight: number) => {
-  return Math.round((((adjustedTotLL / powerLeg) * 100) +
+  if (adjustedTotLL && powerLeg && adjustedTotRL && nonPowerLeg && adjustedPV && adjustedActiveLeft && adjustedActiveRight) {
+    return Math.round((((adjustedTotLL / powerLeg) * 100) +
                     ((adjustedTotRL / nonPowerLeg) * 100) +
                     ((adjustedPV / 100) * 100) +
                     ((adjustedActiveLeft / 100) * 100) +
                     ((adjustedActiveRight / 100) * 100)) / 5)
+  }
+  return 0
 }
 
 const calculateCompletionPercentageAndNextRank = (currentRank: string, totalLeftLeg: number, totalRightLeg: number, isActiveLeft: boolean, isActiveRight: boolean, pvVal: number): {
