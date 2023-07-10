9a114d1e87f5ed2df8ddfea3b8832e62018fcf64
Gerardo Bastidas <gerardo_bastidas@hotmail.com>
2023-07-10 17:39:05 +0200

    add total cv

--- src/components/common/dashBackOffice/CustomerGlobalPool.tsx
+++ src/components/common/dashBackOffice/CustomerGlobalPool.tsx
@@ -2,12 +2,12 @@ import { useState, useEffect, useMemo } from 'react'
 import { PVProgress } from './CustomCircularProgress'
 
 export default function CustomerGlobalPool ({ data }: { data: any}) {
-  const original_value = data?.revenue || 0 / (35 / 100)
-  const shares = Math.floor(original_value / 200)
-  const needed = 200 - Math.floor(original_value - (shares * 200))
+  const totalCV = data?.totalCV || 0
+  const shares = Math.floor(totalCV / 200)
+  const needed = 200 - Math.floor(totalCV - (shares * 200))
   const percents = ((200 - needed) * 100) / 200
   const amounts = {
-    shares: Math.floor(original_value / 200),
+    shares: Math.floor(totalCV / 200),
     needed: needed,
     percents: Math.floor(percents)
   }
@@ -26,6 +26,10 @@ export default function CustomerGlobalPool ({ data }: { data: any}) {
             </div>
             <div className='p-1'>
               <h1 className="text-base text-center pt-2"><strong>{amounts.needed}cv</strong> needed for next share</h1>
+            </div>
+            <div className='p-1'>
+              <h1 className="text-base text-center pt-2">Total CV for the Month</h1>
+              <h1 className="text-base text-center pt-2"><strong>{totalCV}cv</strong></h1>
             </div>
           </div>
         </div>
