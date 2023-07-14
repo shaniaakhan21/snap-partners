58e806bb3a001dc23d89684bb80f81f59802dcc6
amir ali <amiralirang98@gmail.com>
2023-07-14 13:03:09 +0530

    UI for Upline tab added

--- src/components/page/search/individualProfile/Upline.tsx
+++ src/components/page/search/individualProfile/Upline.tsx
@@ -5,13 +5,16 @@ import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
 import axios from 'axios'
 import { getLocalStorage } from 'lib/utils/localStorage'
+import { SpinnerPageContent } from 'components/common/loaders/PageContent'
 
 function Upline ({ id }) {
   const cname = 'user-upline'
   const [UplineData, setUplineData] = useState([])
   const [userId, setUserId] = useState<number>(id)
+  const [isLoading, setIsLoading] = useState(false)
   const token = getLocalStorage('accessToken')
   const getUplineData = async () => {
+    setIsLoading(true)
     await axios.get(`/api/upline/${userId}`, {
       headers: {
         Authorization: `Bearer ${token}`
@@ -20,6 +23,11 @@ function Upline ({ id }) {
       .then((result) => {
         console.log('result from getUplineData', result.data.resultData)
         setUplineData(result.data.resultData)
+        setIsLoading(false)
+      })
+      .catch((e) => {
+        console.log('error while getting upline')
+        setIsLoading(false)
       })
   }
   useEffect(() => {
@@ -27,18 +35,20 @@ function Upline ({ id }) {
   }, [userId])
   return (
     <div className={`${cname}-container`}>
-      { UplineData?.map((data) => (
-        <Card variant='outlined' className={`${cname}-card`}>
-          <CardContent>
-            <p>id-{data?.id}</p>
-            <p>rank-{data?.ranks?.type}</p>
-            <p>name-{data?.name}</p>
-          </CardContent>
-          <CardActions>
-            <button onClick={() => { setUserId(data?.id) }}>see upline</button>
-          </CardActions>
-        </Card>
-      ))
+      {UplineData && !isLoading
+        ? UplineData?.map((data) => (
+          <Card variant='outlined' className={`${cname}-card`}>
+            <CardContent>
+              <p>id-{data?.id}</p>
+              <p>rank-{data?.ranks?.type}</p>
+              <p>name-{data?.name}</p>
+            </CardContent>
+            <CardActions>
+              <button onClick={() => { setUserId(data?.id) }}>see upline</button>
+            </CardActions>
+          </Card>
+        ))
+        : <SpinnerPageContent />
       }
     </div>
   )
