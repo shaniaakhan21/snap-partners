12125147c23f7eccfcea91275c36021ca4eebbb0
aykhanahmadli <botl12020@gmail.com>
2023-07-27 18:12:03 +0400

    Table styles changed

--- src/components/page/erc/PersonalClientsTable.tsx
+++ src/components/page/erc/PersonalClientsTable.tsx
@@ -8,6 +8,26 @@ const PersonalClientsTable = ({ transactions }: ITableTransactionsProps) => {
     { name: "business_name", header: "Company", defaultFlex: 1, minWidth: 110 },
     { name: "email", header: "Email", defaultFlex: 1, minWidth: 85 },
     { name: "phone", header: "Phone", defaultFlex: 1, minWidth: 90 },
+    { name: "details", header: "See More", defaultFlex: 1, minWidth: 90 },
+  ];
+
+  const newtransactions = [
+    {
+      phase: 1,
+      id: 259,
+      business_name: "Xatai",
+      email: "xatai@gmail.com",
+      phone: "419419",
+      details: [],
+    },
+    {
+      phase: 2,
+      id: 259,
+      business_name: "Xatai",
+      email: "xatai@gmail.com",
+      phone: "419419",
+      details: [],
+    },
   ];
 
   const gridStyle = {
@@ -22,12 +42,13 @@ const PersonalClientsTable = ({ transactions }: ITableTransactionsProps) => {
     <ReactDataGrid
       idProperty="id"
       columns={columns}
-      dataSource={transactions}
+      dataSource={newtransactions}
       sortable={true}
       defaultFilterValue={filterValue}
       style={gridStyle}
       defaultLimit={10}
       pagination
+      showEmptyRows={false}
     />
   );
 };
