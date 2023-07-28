1c3c4713e00b8ba5a0edb29bbda4f5b667e5486e
aykhanahmadli <botl12020@gmail.com>
2023-07-28 17:50:24 +0400

    Reusable HeaderComponent for  Tables was created

---
+++ src/components/page/erc/TableHeader.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { TableHeaderProps } from "lib/types/transaction";
+
+const TableHeader: React.FC<TableHeaderProps> = ({
+  tableName,
+  setMonthSelected,
+  setYearSelected,
+}) => {
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const years = [];
+  for (let i = new Date().getFullYear(); i >= 2022; i--) {
+    years.push(i);
+  }
+  return (
+    <div className="flex items-center">
+      <div className="text-lg font-sans font-semibold text-gray-800">
+        {tableName}
+      </div>
+      <select
+        id="legalType"
+        name="legalType"
+        className="ml-5 cursor-pointer relative xs:mr-2 pl-2 pr-12 py-0 xs:py-1 my-2 bg-[rgba(255,255,255,.13)] rounded-md border border-solid border-black outline-none appearance-none leading-8"
+        placeholder="User Rank"
+        onChange={(current) => {
+          setMonthSelected(parseInt(current.target.value));
+        }}
+        style={{ border: "none" }}
+      >
+        {month.map((m, i) => {
+          return (
+            <option key={i} selected={new Date().getMonth() === i} value={i}>
+              {m}
+            </option>
+          );
+        })}
+      </select>
+      <select
+        id="legalType"
+        name="legalType"
+        className="ml-5 cursor-pointer relative xs:mr-2 pl-2 pr-12 py-0 xs:py-1 my-2 bg-[rgba(255,255,255,.13)] rounded-md border border-solid border-black outline-none appearance-none leading-8"
+        placeholder="User Rank"
+        onChange={(current) => {
+          setYearSelected(parseInt(current.target.value));
+        }}
+        style={{ border: "none" }}
+      >
+        {years.map((y, i) => {
+          return (
+            <option key={i} selected={new Date().getFullYear() === y} value={y}>
+              {y}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default TableHeader;
