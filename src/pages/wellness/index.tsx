40649c4e70ae58dc49ea945cfe78c48618ef6847
amir ali <amiralirang98@gmail.com>
2023-09-08 21:57:05 +0400

    referral link redefined

--- src/pages/wellness/index.tsx
+++ src/pages/wellness/index.tsx
@@ -1,17 +1,51 @@
-import { useRef } from 'react'
+/* eslint-disable no-use-before-define */
+
+import React, { useRef } from 'react'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import MainSection from './components/MainSection'
 import ProductTabs from './components/ProductTabs'
+import axios from 'axios'
+import { useRouter } from 'next/router'
 
 const Wellness = () => {
   const productTabsRef = useRef(null)
-
+  const router = useRouter()
   const handleButtonClick = () => {
     if (productTabsRef.current) {
       productTabsRef.current.scrollIntoView({ behavior: 'smooth' })
     }
   }
+
+  // React.useEffect(() => {
+  async function Owner () {
+    // const path = window.location.pathname
+    // if (['/', '/logout'].includes(path)) {
+    //   return
+    // }
+    try {
+      // we remove the first character of the path
+      const { referralCode } = router.query
+      const username = referralCode
+      if (username === 'NoSponsor') {
+        return
+      }
+      console.log('userName', username, router.query)
+      if (username.length > 0) {
+        const response = await axios.get(
+          `/api/integrous/getReplicatedSite?username=${username}`
+        )
+        localStorage.setItem('referralCode', response.data.referralCode)
+        localStorage.setItem('ownerName', `${response.data.name} ${response.data.lastname}`)
+        localStorage.setItem('ownerEmail', `${response.data.email}`)
+        // setUserData(response.data)
+      }
+    } catch (error) {
+      //
+    }
+  }
+  Owner()
+  // }, [])
 
   return (
     <div>
