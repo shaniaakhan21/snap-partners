ab70bb8ec5227a14f77f2d9edbc298d39e9d517b
Hashan Chathuranga <chathuranga.dyrani@gmail.com>
2023-10-04 10:23:48 +0400

    trying to integrate zendesk with the website

--- src/components/page/search/individualProfile/Tickets.tsx
+++ src/components/page/search/individualProfile/Tickets.tsx
@@ -1,9 +1,28 @@
 /* eslint-disable no-use-before-define */
+import axios from 'axios'
 import React, { useEffect } from 'react'
-import Zendesk from 'react-zendesk'
+// import Zendesk from 'react-zendesk'
 const ZENDESK_KEY = '324987dc-ca53-451c-b524-096403f15e91'
 
 const Tickets = () => {
+  const zendeskAPI = axios.create({
+    baseURL: 'https://dyrani.zendesk.com/api/v2', // Replace with your Zendesk subdomain
+    auth: {
+      username: 'amiralirang98@gmail.com', // Replace with your API token or email/password
+      password: 'Amiralirang@123'
+    }
+  })
+
+  // Example: Fetch all tickets
+  const getZendeskData = () => {
+  zendeskAPI.get('/tickets.json')
+    .then((response) => {
+      console.log('Tickets:', response.data)
+    })
+    .catch((error) => {
+      console.error('Error fetching tickets:', error)
+    })
+  }
   const setting = {
     color: {
       theme: '#000'
@@ -23,8 +42,7 @@ const Tickets = () => {
     <div>
       <div>
         {/* Your React component content */}
-        zendesk will be here
-        <Zendesk zendeskKey={ZENDESK_KEY} {...setting} />
+        <button onClick={() => { getZendeskData() }}>click here</button>
       </div>
     </div>
   )
