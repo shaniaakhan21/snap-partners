4d864f22fb2830c649766f0cbff1d330e807b379
Gerardo Bastidas <gerardo_bastidas@hotmail.com>
2023-11-02 14:41:05 +0100

    feat: update rank component

--- src/components/common/overview/RankComponent.tsx
+++ src/components/common/overview/RankComponent.tsx
@@ -55,16 +55,48 @@ interface RankComponentProps{
 export default function RankComponent (props: RankComponentProps) {
   const { data: rankData } = props
   const [value, setValue] = useState(0)
+  const [currentRank, setCurrentRank] = useState(undefined)
 
   useEffect(() => {
     if (rankData) {
       setValue(rankData.currentRankLevel)
+      setCurrentRank(rankData.currentRank)
     }
   }, [rankData])
+
+  const rankColors = new Map()
+  rankColors.set('Free Member', '#DD4C37')
+  rankColors.set('Manager', '#C99FFF')
+  rankColors.set('Supervisor', '#54A52C')
+  rankColors.set('Director', '#F18A00')
+  rankColors.set('Executive', '#000000')
+
+  function formatNumberToLocale (num, decimals) {
+    return num.toLocaleString('en-US', {
+      minimumFractionDigits: decimals,
+      maximumFractionDigits: decimals
+    })
+  }
 
   return (
     <div>
       <Box sx={{ '& .Mui-selected': { color: 'black', bgcolor: 'white' }, '& .MuiTabs-indicator': { backgroundColor: 'transparent' } }}>
+
+        {currentRank && (
+          <div className='bg-white p-2.5 rounded-lg mb-4'>
+            <div>
+              <div className='col-span-3 text-xs pt-2 pb-2 pl-5'>
+                <span className="text-15">Your current rank is:</span>
+              </div>
+              <div className="col-span-2 flex items-center pt-2 pb-2 pl-5">
+                <StyledBox backgroundColor={rankColors.get(rankData.currentRank)}>
+                  <StarIcon style={{ color: '#fff', fontSize: 20, backgroundColor: rankColors.get(rankData.currentRank) }} />
+                </StyledBox>
+                <h1 className="text-2xl text-black font-bold pl-2">{rankData.currentRank}</h1>
+              </div>
+            </div>
+          </div>
+        )}
         <div className='bg-white p-2.5 rounded-lg'>
           <RankSteps currentRank={value} onRankPress={setValue}/>
           <TabPanel value={value} index={0}>
@@ -75,20 +107,6 @@ export default function RankComponent (props: RankComponentProps) {
                   <StarIcon style={{ color: '#fff', fontSize: 20, backgroundColor: '#DD4C37' }} />
                 </StyledBox>
                 <h1 className="text-2xl text-black font-bold pl-2">Free Member</h1>
-              </div>
-              <div className='col-span-3 text-xs pt-5'>
-                <span className="text-15">To become a <strong>Manager</strong> you'll need</span>
-              </div>
-              <div className='col-span-3 text-xs pt-5'>
-                <div className='flex flex-row justify-between'>
-                  <strong className='text-10'>
-                    You have {rankData.mng.commissionVol} PVC
-                  </strong>
-                  <strong className='text-10'>
-                    You need 100 PVC
-                  </strong>
-                </div>
-                <BarWithText progressColor={'#C99FFF'} value={+rankData.mng.commissionVol > 100 ? 100 : +rankData.mng.commissionVol} variant={'determinate'} />
               </div>
               </>
             }
@@ -103,12 +121,38 @@ export default function RankComponent (props: RankComponentProps) {
                 <h1 className="text-2xl text-black font-bold pl-2">Manager</h1>
               </div>
               <div className='col-span-3 text-xs pt-5'>
-                <span className="text-15">To become a <strong>Supervisor</strong> you'll need</span>
+                <span className="text-15">To be a <strong>Manager</strong> you'll need</span>
               </div>
               <div className='col-span-3 text-xs pt-5'>
                 <div className='flex flex-row justify-between'>
                   <strong className='text-10'>
-                    You have {rankData.sv.commissionVol} PVC
+                    You have {formatNumberToLocale(rankData.mng.commissionVol, 0)} PVC
+                  </strong>
+                  <strong className='text-10'>
+                    You need 100 PVC
+                  </strong>
+                </div>
+                <BarWithText progressColor={'#C99FFF'} value={+rankData.mng.commissionVol > 100 ? 100 : +rankData.mng.commissionVol} variant={'determinate'} />
+              </div>
+              </>
+            }
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            {/* Supervisor */}
+            {
+              rankData && <><div className="col-span-2 flex items-center">
+                <StyledBox backgroundColor={'#54A52C'}>
+                  <StarIcon style={{ color: '#fff', fontSize: 20, backgroundColor: '#54A52C' }} />
+                </StyledBox>
+                <h1 className="text-2xl text-black font-bold pl-2">Supervisor</h1>
+              </div>
+              <div className='col-span-3 text-xs pt-5'>
+                <span className="text-15">To be <strong>Supervisor</strong> you'll need</span>
+              </div>
+              <div className='col-span-3 text-xs pt-5'>
+                <div className='flex flex-row justify-between'>
+                  <strong className='text-10'>
+                    You have {formatNumberToLocale(rankData.sv.commissionVol, 0)} PVC
                   </strong>
                   <strong className='text-10'>
                     You need 100 PVC
@@ -127,7 +171,7 @@ export default function RankComponent (props: RankComponentProps) {
                 <span className="text-right ml-auto text-10"><strong>3 Working Legs, 500 V. Each</strong></span>
               </div>
               <div>
-                <div className='grid grid-cols-2 sm:grid-cols-5 md:grid-cols-5 gap-2'>
+                <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-2'>
                   {
                     rankData.sv.workingLegs && Object.values(rankData.sv.workingLegs).map((leg, index) => (
                       <div className='col-span-1'>
@@ -135,15 +179,11 @@ export default function RankComponent (props: RankComponentProps) {
                       </div>
                     ))
                   }
-
-                  <div className='col-span-1 flex justify-end items-end'>
-                    <button className="flex items-center bg-red-600 hover:bg-red-700 text-white font-bold h-6 w-10 py-2 px-4 rounded-l-full rounded-r-full">+</button>
-                  </div>
                 </div>
               </div>
               <div className="flex pt-2">
-                <span className="text-left text-10"><strong>{`Gv You have $${rankData.sv.gv.value}`}</strong></span>
-                <span className="text-right ml-auto text-10"><strong>You need $5000</strong></span>
+                <span className="text-left text-10"><strong>{`GV You have ${formatNumberToLocale(rankData.sv.gv.value, 0)}`}</strong></span>
+                <span className="text-right ml-auto text-10"><strong>You need 5,000</strong></span>
               </div>
               <div className='col-span-1'>
                 <BarWithText progressColor={'#54A52C'} value={+rankData.sv.gv.percentage} variant={'determinate'}/>
@@ -151,22 +191,22 @@ export default function RankComponent (props: RankComponentProps) {
               </>
             }
           </TabPanel>
-          <TabPanel value={value} index={2}>
-            {/* Supervisor */}
+          <TabPanel value={value} index={3}>
+            {/* Director */}
             {
               rankData && <><div className="col-span-2 flex items-center">
-                <StyledBox backgroundColor={'#54A52C'}>
-                  <StarIcon style={{ color: '#fff', fontSize: 20, backgroundColor: '#54A52C' }} />
+                <StyledBox backgroundColor={'#F18A00'}>
+                  <StarIcon style={{ color: '#fff', fontSize: 20, backgroundColor: '#F18A00' }} />
                 </StyledBox>
-                <h1 className="text-2xl text-black font-bold pl-2">Supervisor</h1>
+                <h1 className="text-2xl text-black font-bold pl-2">Director</h1>
               </div>
               <div className='col-span-3 text-xs pt-5'>
-                <span className="text-15">To become a <strong>Director</strong> you'll need</span>
+                <span className="text-15">To be a <strong>Director</strong> you'll need</span>
               </div>
               <div className='col-span-3 text-xs pt-5'>
                 <div className='flex flex-row justify-between'>
                   <strong className='text-10'>
-                    You have {rankData.dct.commissionVol} PVC
+                    You have {formatNumberToLocale(rankData.dct.commissionVol, 0)} PVC
                   </strong>
                   <strong className='text-10'>
                     You need 100 PVC
@@ -181,10 +221,10 @@ export default function RankComponent (props: RankComponentProps) {
               <BarWithText progressColor={'#F18A00'} value={+rankData.dct.PSMPercentage} variant={'determinate'}/>
               <div className="flex pt-2">
                 <span className="text-left text-10"><strong>You need</strong></span>
-                <span className="text-right ml-auto text-10"><strong>4 Working Legs, 5000 V. Each</strong></span>
+                <span className="text-right ml-auto text-10"><strong>4 Working Legs, 5,000 V. Each</strong></span>
               </div>
               <div>
-                <div className='grid grid-cols-2 sm:grid-cols-6 md:grid-cols-6 gap-2'>
+                <div className='grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 gap-2'>
                   {
                     rankData.dct.workingLegs && Object.values(rankData.dct.workingLegs).map((leg, index) => (
                       <div className='col-span-1'>
@@ -192,30 +232,27 @@ export default function RankComponent (props: RankComponentProps) {
                       </div>
                     ))
                   }
-
-                  <div className='col-span-1 flex justify-end items-end'>
-                    <button className="flex items-center bg-red-600 hover:bg-red-700 text-white font-bold h-6 w-10 py-2 px-4 rounded-l-full rounded-r-full">+</button>
-                  </div>
                 </div>
               </div>
               <div className="flex pt-2">
-                <span className="text-left text-10"><strong>{`Gv You have $${rankData.dct.gv.value}`}</strong></span>
-                <span className="text-right ml-auto text-10"><strong>You need $50000</strong></span>
+                <span className="text-left text-10"><strong>{`GV You have ${formatNumberToLocale(rankData.dct.gv.value, 0)}`}</strong></span>
+                <span className="text-right ml-auto text-10"><strong>You need 50,000</strong></span>
               </div>
               <div className='col-span-1'>
                 <BarWithText progressColor={'#F18A00'} value={+rankData.dct.gv.percentage} variant={'determinate'}/>
               </div>
+
               </>
             }
           </TabPanel>
-          <TabPanel value={value} index={3}>
-            {/* Director */}
+          <TabPanel value={value} index={4}>
+            {/* Executive */}
             {
               rankData && <><div className="col-span-2 flex items-center">
-                <StyledBox backgroundColor={'#F18A00'}>
-                  <StarIcon style={{ color: '#fff', fontSize: 20, backgroundColor: '#F18A00' }} />
+                <StyledBox backgroundColor={'#000000'}>
+                  <StarIcon style={{ color: '#fff', fontSize: 20, backgroundColor: '#000000' }} />
                 </StyledBox>
-                <h1 className="text-2xl text-black font-bold pl-2">Director</h1>
+                <h1 className="text-2xl text-black font-bold pl-2">Executive</h1>
               </div>
               <div className='col-span-3 text-xs pt-5'>
                 <span className="text-15">To be a <strong>Executive</strong> you need</span>
@@ -238,10 +275,10 @@ export default function RankComponent (props: RankComponentProps) {
               <BarWithText progressColor={'#000000'} value={+rankData.exec.PSMPercentage} variant={'determinate'}/>
               <div className="flex pt-2">
                 <span className="text-left text-10"><strong>You need</strong></span>
-                <span className="text-right ml-auto text-10"><strong>5 Working Legs, 10000 V. Each</strong></span>
+                <span className="text-right ml-auto text-10"><strong>5 Working Legs, 10,000 V. Each</strong></span>
               </div>
               <div>
-                <div className='grid grid-cols-2 sm:grid-cols-6 md:grid-cols-6 gap-2'>
+                <div className='grid grid-cols-2 sm:grid-cols-5 md:grid-cols-5 gap-2'>
                   {
                     rankData.exec.workingLegs && Object.values(rankData.exec.workingLegs).map((leg, index) => (
                       <div className='col-span-1'>
@@ -249,30 +286,14 @@ export default function RankComponent (props: RankComponentProps) {
                       </div>
                     ))
                   }
-
-                  <div className='col-span-1 flex justify-end items-end'>
-                    <button className="flex items-center bg-red-600 hover:bg-red-700 text-white font-bold h-6 w-10 py-2 px-4 rounded-l-full rounded-r-full">+</button>
-                  </div>
                 </div>
               </div>
               <div className="flex pt-2">
-                <span className="text-left text-10"><strong>{`Gv You have $${rankData.exec.gv.value}`}</strong></span>
-                <span className="text-right ml-auto text-10"><strong>You need $100000</strong></span>
+                <span className="text-left text-10"><strong>{`GV You have ${formatNumberToLocale(rankData.exec.gv.value, 0)}`}</strong></span>
+                <span className="text-right ml-auto text-10"><strong>You need 100,000</strong></span>
               </div>
               <div className='col-span-1'>
                 <BarWithText progressColor={'#000000'} value={+rankData.exec.gv.percentage} variant={'determinate'}/>
-              </div>
-              </>
-            }
-          </TabPanel>
-          <TabPanel value={value} index={4}>
-            {/* Executive */}
-            {
-              rankData && <><div className="col-span-2 flex items-center">
-                <StyledBox backgroundColor={'#000000'}>
-                  <StarIcon style={{ color: '#fff', fontSize: 20, backgroundColor: '#000000' }} />
-                </StyledBox>
-                <h1 className="text-2xl text-black font-bold pl-2">Executive</h1>
               </div>
               </>
             }
