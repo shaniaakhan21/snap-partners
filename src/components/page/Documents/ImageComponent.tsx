e5a41087012d7556b4cf2d65de5d74a1302931b2
Hashan Chathuranga <chathuranga.dyrani@gmail.com>
2023-11-03 16:25:17 +0400

    thumbnail of document now displaying from s3 url

--- src/components/page/Documents/ImageComponent.tsx
+++ src/components/page/Documents/ImageComponent.tsx
@@ -7,14 +7,15 @@ interface file {
 function ImageComponent (props:file) {
   const { fileId } = props
   const [imgUrl, setImgUrl] = useState('')
+  const[thumbnailData, setThumbnailData] = useState(null)
   const fetchThumbnailUrl = async (fileId) => {
     console.log('in fetch function')
     await axios.get(`/api/document/thumbnail/${fileId}`)
       .then(async (response) => {
         if (response.data.status) {
-          const res = await (getFileSrc(response.data.result[0], true))
-          console.log('res is', res)
-          setImgUrl(res)
+          // const res = await (getFileSrc(response.data.result[0], true))
+          console.log('res is thumbnail', response.data.result[0])
+          setThumbnailData(response.data.result[0])
         } else {
           return ''
         }
@@ -34,7 +35,7 @@ function ImageComponent (props:file) {
     return (URL.createObjectURL(data))
   }
   return (
-    <div><img src={imgUrl} style={{
+    <div><img src={thumbnailData && thumbnailData.thumbnailUrl ? thumbnailData.thumbnailUrl : `https://snap-delivered.nyc3.digitaloceanspaces.com/documentsThumbnail/${encodeURIComponent(thumbnailData?.thumbnailName)}`} style={{
         width: '100%',
         height: '150px',
         objectFit: 'contain'
