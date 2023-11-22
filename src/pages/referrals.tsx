94086a6e676f4cd832cc77de408105e5c0fc3d9b
unknown <shaniaakhan21@gmail.com>
2023-11-22 11:20:56 +0400

    owner issue fixed

--- src/pages/referrals.tsx
+++ src/pages/referrals.tsx
@@ -34,10 +34,6 @@ const ReferralsPage: Page = () => {
   return (
     <div className='min-h-[80vh] flex justify-center items-center'>
       <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 items-center justify-center justify-items-center gap-4'>
-
-        {(auth.roles.customer && !auth.roles.admin) && (
-          <h3> No Links Available</h3>
-        )}
         {(auth.roles.driver || auth.roles.admin || auth.roles.merchant || auth.roles.ibo || auth.roles.integrousAssociate) && (
           <ReferralCards
             title='Sponsor a Snap IBO'
