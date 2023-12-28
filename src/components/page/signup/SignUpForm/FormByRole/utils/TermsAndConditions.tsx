e62b9f2c95b74e5a06408539222d6092e792bb79
Shaniya Khan <57680699+shaniaakhan21@users.noreply.github.com>
2023-12-28 15:23:16 +0400

    Fixed terms

--- src/components/page/signup/SignUpForm/FormByRole/utils/TermsAndConditions.tsx
+++ src/components/page/signup/SignUpForm/FormByRole/utils/TermsAndConditions.tsx
@@ -1,15 +1,20 @@
-
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { FieldErrors, UseFormRegister } from 'react-hook-form'
 
 interface IInputFormProps {
   register: UseFormRegister<any>,
   errors: any,
   rulesForm: FieldErrors,
-  referralLink?: any
 }
 
-export const TermsAndConditions = ({ register, errors, rulesForm, referralLink }: IInputFormProps) => {
+export const TermsAndConditions = ({ register, errors, rulesForm }: IInputFormProps) => {
+  const router = useRouter()
+  const { role } = router.query
+  const termsLink = role === 'IBO'
+    ? '/legal/terms-of-use-ibo'
+    : '/legal/terms-of-use'
+
   return (
     <section className='space-y-2 font-semibold text-gray-600 text-sm sm:text-base'>
       <div className='flex items-center justify-start'>
@@ -24,7 +29,7 @@ export const TermsAndConditions = ({ register, errors, rulesForm, referralLink }
 
         <label htmlFor='termsAndConditions' className='ml-2 text-md'>
         Agree to {' '}
-          <Link href= { referralLink.role === 'AGENT' || referralLink.role === 'IBO' ? '/legal/terms-of-use-ibo' : '/legal/terms-of-use'} >
+          <Link href= {termsLink} >
             <a className='text-textAcent-500 font-semibold focus:underline'>Terms and Conditions</a>
           </Link>
         </label>
