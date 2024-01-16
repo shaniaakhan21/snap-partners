53cae5e2064a1dce88fcef3bc88e1781d238a6f9
unknown <shaniaakhan21@gmail.com>
2024-01-16 11:30:22 +0400

    Redesign Training

--- src/components/page/training/CategoryChipList.tsx
+++ src/components/page/training/CategoryChipList.tsx
@@ -1,7 +1,9 @@
 import { ReactNode } from 'react'
 
 export const CategoryChipList = ({ children }: { children: ReactNode }) => (
-  <ul className='flex flex-wrap justify-center'>
-    {children}
-  </ul>
+  <div className='flex justify-center'>
+    <ul className='flex flex-col md:flex-row flex-nowrap md:flex-wrap justify-center w-fit  shadow-[0_1px_17px_-1px_rgba(0,0,0,0.2)] md:rounded-3xl bg-white'>
+      {children}
+    </ul>
+  </div>
 )
