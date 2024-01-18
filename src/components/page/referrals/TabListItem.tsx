455f65c70ce119e7c58d6a244c989a2dc4ff1063
Shaniya Khan <57680699+shaniaakhan21@users.noreply.github.com>
2024-01-18 15:32:16 +0400

    GENEALOGY DONE

--- src/components/page/referrals/TabListItem.tsx
+++ src/components/page/referrals/TabListItem.tsx
@@ -11,22 +11,21 @@ interface IProps {
 
 export const ReferralTabListItem = ({ isSelect = false, id, newUsers, numUsers, noCursor = false, onClick = () => {} }: IProps) => {
   return (
-    <li className={`bg-white rounded-sm relative mb-4 h-24 list-none last:mb-0 ${noCursor ? '' : 'shadow'}`}>
-      {isSelect && <div style={{ top: '30px' }} className='absolute left-2 rounded-full bg-primary-500 w-2 h-2' />}
+    <li className={`bg-white rounded-tl-2xl relative mb-4 h-24 list-none last:mb-0 ${noCursor ? '' : 'shadow'}`}>
+      {isSelect && <div style={{ top: '35px' }} className='absolute left-2 rounded-full bg-primary-500 w-2 h-6' />}
       <button
         style={{ cursor: noCursor ? 'auto' : 'pointer' }}
         className={`border border-transparent rounded-sm w-full h-full py-2 pl-5 pr-2 text-sm text-left ${noCursor ? '' : 'transition-colors hover:border-primary-500'}`}
         onClick={() => onClick(id)}
       >
-        <div className='flex items-center justify-between'>
-          <span className='text-textAcent-500'>Level {id}</span>
+        <div className='flex items-center justify-start'>
+          <span className='text-black text-xl font-bold'>Level {id}</span>
+          <div className='block my-1 text-[#7194AF] text-lg font-semibold'><span className="py-1 px-4 mx-2 font-bold rounded-lg text-primary-500 border border-primary-500">{numUsers}</span> Users</div>
           {/* <div className='flex items-center justify-center'>
             <EyeVisibleIcon classes='w-5 h-5 fill-[#777777]' />
             <span className='text-textAcent-500 ml-1'>See income</span>
           </div> */}
         </div>
-
-        <span className='block my-1'>{numUsers} Users</span>
 
         {/* <span className={`block ${newUsers > 0 ? 'text-success-500' : ''}`}>{newUsers} New Users</span> */}
       </button>
