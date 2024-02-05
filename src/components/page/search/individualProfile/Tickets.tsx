bf6525d095e8ff5b5e22cbf8090d66adeb43b6ec
Hashan Chathuranga <chathuranga.dyrani@gmail.com>
2024-02-05 17:28:38 +0400

    tickets Added to admin section

--- src/components/page/search/individualProfile/Tickets.tsx
+++ src/components/page/search/individualProfile/Tickets.tsx
@@ -1,236 +1,3 @@
-// /* eslint-disable no-use-before-define */
-// import axios from 'axios'
-// import React, { useEffect, useState } from 'react'
-// import ZendeskTicketCreation from './modalPopups/zendesk/ZendeskTicketCreation'
-// import { ButtonComponent, InputComponent, SelectComponent } from 'components/layout/private/Dashboard/Navbar/adminTools/searchForms/Components'
-// import { DataGrid as MUIDataGrid } from '@mui/x-data-grid'
-// import { styled } from '@mui/system'
-// // import Zendesk from 'react-zendesk'
-// const ZENDESK_KEY = '324987dc-ca53-451c-b524-096403f15e91'
-
-// const StyledDataGrid = styled(MUIDataGrid)(() => ({
-//   '&& .MuiDataGrid-columnHeaderTitleContainer .MuiDataGrid-columnHeaderTitle': {
-//     fontWeight: 'bold',
-//     fontSize: '1.2em'
-//   },
-//   '& .MuiDataGrid-cell': {
-//     borderColor: 'rgba(224, 224, 224, 0.5)!important'
-//   },
-
-//   '& .MuiDataGrid-columnHeaders': {
-//     backgroundColor: '#FBFDFF',
-//     borderColor: 'rgba(224, 224, 224, 0.5)!important'
-//   },
-//   '& .MuiDataGrid-footerContainer': {
-//     display: 'none'
-//   },
-//   '& .MuiDataGrid-virtualScroller': {
-//     overflow: 'hidden'
-//   }
-
-// }))
-
-// const rows = [
-//   {
-//     clientName: 'Edwin Zam',
-//     status: 'open',
-//     subject: '12365',
-//     dayCreated: '2023-05-21',
-//     lastUpdated: '2023-05-21',
-//     lastResponse: 'Anndre M',
-//     comments: 4,
-//     id: 1
-//   },
-//   {
-//     clientName: 'Niel Goldman',
-//     status: 'closed',
-//     subject: '12365',
-//     dayCreated: '2023-05-21',
-//     lastUpdated: '2023-05-2',
-//     lastResponse: 'Anndre M',
-//     comments: 4,
-//     id: 2
-//   }
-// ]
-
-// const Tickets = ({ zendesk_id, name, email }) => {
-//   const [windowWidth, setWindowWidth] = useState(0)
-//   const [tickets, setTickets] = useState([])
-//   const [ticketFlag, setTicketFlag] = useState(false)
-
-//   useEffect(() => {
-//     const handleResize = () => {
-//       setWindowWidth(window.innerWidth)
-//     }
-
-//     window.addEventListener('resize', handleResize)
-
-//     return () => {
-//       window.removeEventListener('resize', handleResize)
-//     }
-//   }, [])
-//   useEffect(() => {
-//     getZendeskData()
-//   }, [ticketFlag])
-
-//   const columns = [
-//     {
-//       field: 'status',
-//       headerName: 'Status',
-//       flex: windowWidth <= 400 ? 0.5 : 1,
-//       renderCell: (params) => {
-//         const value = params.value
-//         const cellStyle = {
-//           padding: '3% 10%',
-//           color: 'white',
-//           fontSize: '1.2em',
-//           backgroundColor: value === 'open' ? '#FFB7AD' : value === 'solved' ? '#A2E85D' : 'black'
-//         }
-
-//         return <div style={cellStyle}>{`${value}`}</div>
-//       }
-//     },
-//     {
-//       field: 'id',
-//       headerName: 'ID'
-//       // flex: windowWidth <= 400 ? 0.5 : 1
-//     },
-//     {
-//       field: 'clientName',
-//       headerName: 'Client Name',
-//       flex: windowWidth <= 400 ? 0.5 : 1
-//     },
-//     {
-//       field: 'subject',
-//       headerName: 'Subject',
-//       flex: windowWidth <= 400 ? 0.5 : 1
-//     },
-//     {
-//       field: 'dayCreated',
-//       headerName: 'DayCreated',
-//       type: 'string',
-//       flex: windowWidth <= 400 ? 0.5 : 1
-//     },
-//     {
-//       field: 'lastUpdated',
-//       headerName: 'Last Updated',
-//       flex: windowWidth <= 400 ? 0.5 : 1
-//     },
-//     {
-//       field: 'lastResponse',
-//       headerName: 'Last Response',
-//       flex: windowWidth <= 400 ? 0.5 : 1
-//     },
-//     {
-//       field: 'comments',
-//       headerName: 'Comments',
-//       flex: windowWidth <= 400 ? 0.5 : 1
-//     }
-//   ]
-
-//   const zendeskAPI = axios.create({
-//     baseURL: 'https://amirco7782.zendesk.com/api/v2', // Replace with your Zendesk subdomain
-//     auth: {
-//       username: 'next100x98@gmail.com', // Replace with your API token or email/password
-//       password: 'Amirali@123'
-//     }
-//   })
-
-//   const [zendeskTicketModal, setZendeskTicketModal] = useState(false)
-
-//   // Example: Fetch all tickets
-//   const getZendeskData = () => {
-//     axios.get('/api/zendesk/ticket')
-//       .then(async (response) => {
-//         setTickets(await Promise.all(response.data.response.tickets.map(async (ticket) => {
-//           const requesterData = await axios.get('/api/zendesk/requester', { params: { requester_id: ticket.requester_id } })
-//           const requesterName = await requesterData.data.response.user.name
-//           return {
-//             clientName: requesterName,
-//             status: ticket.status,
-//             subject: ticket.raw_subject,
-//             dayCreated: ticket.created_at,
-//             lastUpdated: ticket.updated_at,
-//             lastResponse: 'Anndre M',
-//             comments: ticket.comment_count,
-//             id: ticket.id
-//           }
-//         }))
-//       )
-//       })
-//       .catch((error) => {
-//         console.error('Error fetching tickets:', error)
-//       })
-//   }
-
-//   const onZendeskTicketModalClose = () => {
-//     setZendeskTicketModal(false)
-//   }
-
-//   const setting = {
-//     color: {
-//       theme: '#000'
-//     },
-//     launcher: {
-//       chatLabel: {
-//         'en-US': 'Need Help'
-//       }
-//     },
-//     contactForm: {
-//       fields: [
-//         { id: 'description', prefill: { '*': 'My pre-filled description' } }
-//       ]
-//     }
-//   }
-//   return (
-//     <div>
-//       <div className='flex flex-row justify-between'>
-//         <div className='flex flex-row gap-2'>
-//           <img src='/images/message-circle.svg' style={{ width: '50px', height: '50px' }} />
-//           <span className='mt-3 text-[#E35C49] font-open-sans text-xl font-semibold leading-6'>Help Tickets</span>
-//         </div>
-//         <div>
-//           <button onClick = {() => { setZendeskTicketModal(true) }} className= {'bg-[#2C7D0E] text-white px-3 py-2'}><span className='text-[#FFF] text-center font-open-sans text-sm font-bold leading-4 uppercase'>CREATE NEW TICKET</span></button>
-//         </div>
-//       </div>
-//       <div className='flex flex-row justify-evenly mt-10 gap-3'>
-//         <div className='searchForm-inputContainer'>
-//           <InputComponent label={'search ticket'} placeholder={'ID, Name, Status'} />
-//         </div>
-//         <div className='searchForm-ButtonContainer'>
-//           <ButtonComponent title={'search'} />
-//         </div>
-//       </div>
-//       <div>
-//         <p className='text-[#404040] font-open-sans text-xl font-semibold leading-6 mt-10'>Friday, June 21, 2019</p>
-//       </div>
-//       <div className='mt-10'>
-//         <StyledDataGrid rows={tickets && tickets}
-//           columns={columns}
-//           sx={{
-//             minHeight: '214px',
-//             borderColor: 'rgba(224, 224, 224, 0.5)!important'
-//           }}/>
-//       </div>
-//       {/* <div>
-//         <button onClick={() => { getZendeskData() }}>click here</button>
-//       </div> */}
-
-//       <ZendeskTicketCreation zendeskTicketModal={zendeskTicketModal} closeModal = {onZendeskTicketModalClose} ticketFlag={ticketFlag} setTicketFlag={setTicketFlag} zendesk_id={zendesk_id} name= {name} email= {email} />
-//     </div>
-//   )
-// }
-
-// export default Tickets
-
-
-
-
-
-
-
-/* eslint-disable @next/next/no-document-import-in-page */
-/* eslint-disable no-use-before-define */
 import axios from 'axios'
 import React, { useEffect, useRef, useState } from 'react'
 import ZendeskTicketCreation from 'components/page/search/individualProfile/modalPopups/zendesk/ZendeskTicketCreation'
@@ -245,7 +12,6 @@ import { APP_INFO } from 'config/appInfo'
 import { useAuthStore } from 'lib/stores'
 import { Spinner } from 'components/common/loaders'
 // import Zendesk from 'react-zendesk'
-const ZENDESK_KEY = '324987dc-ca53-451c-b524-096403f15e91'
 
 const StyledDataGrid = styled(MUIDataGrid)(() => ({
   '&& .MuiDataGrid-columnHeaderTitleContainer .MuiDataGrid-columnHeaderTitle': {
@@ -268,32 +34,7 @@ const StyledDataGrid = styled(MUIDataGrid)(() => ({
   }
 
 }))
-const { SEO } = APP_INFO
-
-const rows = [
-  {
-    clientName: 'Edwin Zam',
-    status: 'open',
-    subject: '12365',
-    dayCreated: '2023-05-21',
-    lastUpdated: '2023-05-21',
-    lastResponse: 'Anndre M',
-    comments: 4,
-    id: 1
-  },
-  {
-    clientName: 'Niel Goldman',
-    status: 'closed',
-    subject: '12365',
-    dayCreated: '2023-05-21',
-    lastUpdated: '2023-05-2',
-    lastResponse: 'Anndre M',
-    comments: 4,
-    id: 2
-  }
-]
-
-const Tickets = () => {
+const Tickets = ({ zendesk_id, name, email }) => {
   const [windowWidth, setWindowWidth] = useState(0)
   const [allTickets, setAllTickets] = useState([])
   const [tickets, setTickets] = useState([])
@@ -374,14 +115,6 @@ const Tickets = () => {
     }
   ]
 
-  const zendeskAPI = axios.create({
-    baseURL: 'https://amirco7782.zendesk.com/api/v2', // Replace with your Zendesk subdomain
-    auth: {
-      username: 'next100x98@gmail.com', // Replace with your API token or email/password
-      password: 'Amirali@123'
-    }
-  })
-
   const [zendeskTicketModal, setZendeskTicketModal] = useState(false)
   const [zendeskChatOpen, setZendeskChatOpen] = useState(false)
   const [loading, setLoading] = useState(false)
@@ -392,7 +125,7 @@ const Tickets = () => {
     setLoading(true)
     axios.get('/api/zendesk/ticket', {
       params: {
-        zendesk_id: auth?.zendesk_id
+        zendesk_id: zendesk_id
       }
     })
       .then(async (response) => {
