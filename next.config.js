e21f2a970911fe74830c3ab3d8df6125fb9db47b
Hashan Chathuranga <chathuranga.dyrani@gmail.com>
2024-02-06 11:04:35 +0400

    config file corrected

--- next.config.js
+++ next.config.js
@@ -8,8 +8,8 @@ const nextConfig = {
       ? [
         {
           source: '/api/:path*',
-          destination: `http://localhost:8080/:path*`
-          // destination: 'https://snapdeliveredteam.com/api/:path*'
+          // destination: `http://localhost:8080/:path*`
+          destination: 'https://snapdeliveredteam.com/api/:path*'
           // destination: 'https://zendeskintegrationnew.snap.devopsteam.info/api/:path*'
         }
       ]
